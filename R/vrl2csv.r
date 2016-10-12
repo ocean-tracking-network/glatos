7767eebe80aac962b020f5179ea791dd62f3d893
Chris Holbrook <cholbrook@usgs.gov>
2016-10-12 10:10:05 -0400

    cd "C:\\Program Files (x86)\\Vemco\\VUE\" & VUE --convert-files --output-format csv C:/holbrook/myR/workingPackages_examples/glatos/vemco_xml_testing/data/VR2W180_301577_20160718_1.vrl

--- R/vrl2csv.r
+++ R/vrl2csv.r
@@ -3,6 +3,7 @@
 
 vrl2csv <- function(vrlDir, vueExePath="C:\\Program Files (x86)\\Vemco\\VUE"){
   if( !("VUE.exe" %in% list.files(vueExePath))) stop("VUE.exe not found at specified path.")
-  shelltxt <- paste("cd \"",vueExePath,"\\\"", " & VUE --convert-files --output-format csv ", vrlDir, "\\*.vrl", sep="")
+  shelltxt <- paste0("cd \"",vueExePath,"\\\"", " & VUE --convert-files --output-format csv ", vrlDir, "\\*.vrl")
   shell(shelltxt)
+  return(gsub(".vrl|.VRL",".csv",list.files(vrlDir,full.names=T,pattern="vrl$|VRL$")))
 }
