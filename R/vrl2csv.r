34c2e530dac0ee00a7c38a971edd9f46ea4b1e44
Chris Holbrook <cholbrook@usgs.gov>
2016-10-12 10:47:02 -0400

    allow folder or files as input to vrl2ulfx; also minor updates to vrl2csv

--- R/vrl2csv.r
+++ R/vrl2csv.r
@@ -1,25 +1,20 @@
-#Convert VRL file to CSV
-#This creates a CSV file for each VRL without direct user interaction with VUE
-
 vrl2csv <- function(vrl, vueExePath="C:\\Program Files (x86)\\Vemco\\VUE"){
   #check path to VUE.exe
   if( !("VUE.exe" %in% list.files(vueExePath))) stop("VUE.exe not found at specified path.")
   
   #if vrl is single directory, get list of vrl file names
   if(all(file.info(vrl)$isdir)) {
-	if(!file.exists(vrl)) stop(paste0("File or folder ",vrl," not found.")
+	if(!file.exists(vrl)) stop(paste0("File or folder ",vrl," not found."))
 	if(length(vrl) > 1) stop("input argument 'vrl' cannot include more than one directory")
 	if(length(vrl) == 0) stop(paste0("No VRL files found at ",vrl))
 	if(length(vrl) == 1) vrl <- list.files(vrl,full.names=T, pattern=".vrl$|.VRL$|.Vrl$")
   } #end if
   
-  #call vue command
-  
+  #invoke vue command
   shelltxt <- paste0("cd \"",vueExePath,"\\\"", " & VUE --convert-files --output-format csv ", 
 	paste(vrl,collapse=" "))
- # shelltxt <- paste0("cd \"",vueExePath,"\\\"", " & VUE --convert-files --output-format csv ", vrlDir, "\\*.vrl")
   shell(shelltxt)
   
   #return output vrl path(s) and file name(s)
-  return(vrl)
+  return(gsub(".vrl|.VRL|.Vrl",".ulfx",vrl))
 }
