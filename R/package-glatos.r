8febe9eabdf6c310df956ba4705d20d3820b1b68
Christopher Holbrook <cholbrook@usgs.gov>
2017-02-22 01:25:53 -0500

    update documentation

--- R/package-glatos.r
+++ R/package-glatos.r
@@ -1,7 +1,7 @@
 #' glatos: A package for the Great Lakes Acoustic Telemetry Observation System
 #'
 #' The glatos package provides three categories of important functions:
-#' simulation, visualization, and random utility functions.
+#' simulation, processing and summarization, visualization and exploration, and random utility functions.
 #' 
 #' @section Simulation functions:
 #' The function \code{\link{calcCollisionProb}} is useful for determing the
@@ -12,9 +12,20 @@
 #'   \code{\link{detectTransmissions}} can be used to explore theoretical 
 #'   performance of custom receiver network arrangements. \cr\cr
 #'
-#' @section Visualization functions:
+#' @section Processing and Summarization functions:
+#' The function \code{\link{falseDetectionFilter}} identifies potential 
+#'   false detections using the GLATOS min_lag column. \cr\cr
+#' The function \code{\link{detectionEventFilter}} distills detection data
+#'   down to a much smaller number of discrete detection events. \cr\cr
+#'
+#' @section Visualization and Exploration functions:
 #' The function \code{\link{kmlWorkbook}} is useful for exploring receiver and 
 #'   animal release locations in Google Earth. \cr\cr
+#' The functions \code{\link{abacusPlot}} and \code{\link{detectionEventPlot}} 
+#'   are useful for exploring movement patterns of individual tagged animals.
+#' \cr\cr
+#' The function \code{\link{detectionBubblePlot}} is useful for exploring  
+#'   distribution of tagged individuals among receivers. \cr\cr
 #'
 #' @section Random Utility functions:
 #' The functions \code{\link{vrl2csv}}, \code{\link{rotatePoints}},
