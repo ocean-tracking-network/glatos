bba80f28c5c08cc91d43adae7c0073c2b532374c
Christopher Holbrook <cholbrook@usgs.gov>
2017-02-25 00:04:53 -0500

    ref sp package in call to point.in.polygon in crwInPolygon and edit help

--- R/crwInPolygon.r
+++ R/crwInPolygon.r
@@ -4,12 +4,17 @@
 #'   with turning angles drawn from a normal distribution inside a polygon.
 #'
 #' @param polyg A polygon defined as data frame with columns x and y.
+#' 
 #' @param theta A 2-element numeric vector with turn angle parameters 
 #'   (theta[1] = mean; theta[2] = sd) from normal distribution.
+#'   
 #' @param stepLen A numeric scalar with total distance moved in each step.
+#' 
 #' @param initPos A 2-element numeric vector with nital position (initPos[1]=x, 
 #'   initPos[2]=y).
+#'   
 #' @param initHeading A numeric scalar with initial heading in degrees.
+#' 
 #' @param nsteps A numeric scalar with number of steps to simulate.
 #'
 #' @details
@@ -38,32 +43,30 @@
 #' mypolygon <- data.frame(x=c(-50,-50, 50, 50),y=c(-50,50,50,-50))
 #' foo <- crwInPolygon(mypolygon,theta=c(0,5), stepLen=10, initPos=c(0,0),
 #'   initHeading=0, nsteps=50)
-#' plot(foo,type="b",pch=20,cex=0.2,asp=c(1,1),xlim=range(mypolygon$x),
+#' plot(foo,type="o",pch=20,asp=c(1,1),xlim=range(mypolygon$x),
 #'   ylim=range(mypolygon$y))
 #' polygon(mypolygon)
 #'
 #' @export
- crwInPolygon <- function(polyg=data.frame(x=c(0,0,150000,150000),y=c(0,50000,50000,0)),
-              theta=c(0,10), 
-              stepLen=100, 
-              initPos=c(NA,NA), 
-              initHeading=NA,
-			  nsteps=30){          
+ crwInPolygon <- function(polyg=data.frame(x=c(0,0,150000,150000),
+   y=c(0,50000,50000,0)), theta=c(0,10), stepLen=100, initPos=c(NA,NA), 
+   initHeading=NA, nsteps=30){          
               			  
-      #randomly select one point in the study area
-      if(any(is.na(initPos))){
-		  inPoly <- FALSE #logical flag; preallocate
-		  while(inPoly == FALSE){
-			   init <- c(runif(1, min(polyg$x),max(polyg$x)),
-					runif(1, min(polyg$y),max(polyg$y)))
-			   inPoly <- point.in.polygon(init[1],init[2],polyg$x,polyg$y)
-			   inPoly <- switch(inPoly+1,FALSE,TRUE,FALSE,FALSE)
+   #randomly select one point in the study area
+   if(any(is.na(initPos))){
+		 inPoly <- FALSE #logical flag; preallocate
+		 while(inPoly == FALSE){
+			 init <- c(runif(1, min(polyg$x),max(polyg$x)),
+			 runif(1, min(polyg$y),max(polyg$y)))
+			 inPoly <- sp::point.in.polygon(init[1],init[2],polyg$x,polyg$y)
+		   inPoly <- switch(inPoly+1,FALSE,TRUE,FALSE,FALSE)
 		  } #end while
 	  } #end if
 	  
 	  if(all(!is.na(initPos))) {
 		init <- initPos
-		if(point.in.polygon(init[1],init[2],polyg$x,polyg$y)==0) stop("initPos is outside polygon boundary.")
+		if(sp::point.in.polygon(init[1],init[2],polyg$x,polyg$y)==0) 
+		  stop("initPos is outside polygon boundary.")
 	  } #end if
 	  
 	  #randomly select heading
@@ -79,7 +82,7 @@
 	  rows.i <- 1 #rows for ith window iteration; preallocate
 		
 	  #initialize progress bar
-	  pb <- txtProgressBar(title="Status of current path", label="0% done", min=0, max=mod+1, initial=0)	  
+	  pb <- txtProgressBar(min=0, max=mod+1, initial=0, style=3)	  
 
 	  nwin <- mod+(rmd>0) #number of windows
 	  for(i in 1:nwin){
@@ -87,7 +90,8 @@
 		  #update starting point and heading
 		  init <- as.vector(path.fwd[max(rows.i),]) #start at previous end
 		  if(i>1) {
-			initHeading <- vectorHeading(path.fwd$x[max(rows.i)-1:0],path.fwd$y[max(rows.i)-1:0])
+			initHeading <- vectorHeading(path.fwd$x[max(rows.i)-1:0],
+			  path.fwd$y[max(rows.i)-1:0])
 		  }
 		  if(i==nwin & rmd>0) winSz <- rmd #size of last window
 		  rows.i <- max(rows.i)+(1:winSz) #update rows for this iteration
@@ -95,7 +99,8 @@
 				   initHeading,nsteps=winSz)
 
 		  #check if in polygon
-		  inPoly <- point.in.polygon(path.fwd$x[rows.i],path.fwd$y[rows.i],polyg$x,polyg$y)
+		  inPoly <- sp::point.in.polygon(path.fwd$x[rows.i],path.fwd$y[rows.i],
+		    polyg$x,polyg$y)
 
 		  while(!all(inPoly)){
 			  outside <- match(0,inPoly) #identify first step outside
@@ -109,7 +114,7 @@
 					ylen <- cos(heading_rad)*len #y-component vector
 					bar <- data.frame(x=x[2]+xlen,y=y[2]+ylen,theta=-180:180)
 					#check if sampled points in polygon
-					bar$inPoly <- point.in.polygon(bar$x,bar$y,polyg$x,polyg$y)
+					bar$inPoly <- sp::point.in.polygon(bar$x,bar$y,polyg$x,polyg$y)
 					if(sum(bar$inPoly) == 0) stop("Path stuck at boundary.")
 					probs <- bar$inPoly*dnorm(-180:180,theta[1],theta[2])
 					theta2 <- sample(bar$theta,1,prob=probs)
@@ -121,19 +126,18 @@
 			  
 			  #rotate track from first step outside polygon to end
 			  path.fwd[rows.i,][outside:length(rows.i),] <- rotatePoints(
-				x=path.fwd$x[rows.i][outside:length(rows.i)],
-				y=path.fwd$y[rows.i][outside:length(rows.i)],
-				theta=theta2,
-				focus=path.fwd[rows.i[outside]-1,])
+				  x=path.fwd$x[rows.i][outside:length(rows.i)],
+				  y=path.fwd$y[rows.i][outside:length(rows.i)],
+				  theta=theta2, focus=path.fwd[rows.i[outside]-1,])
 			  
 			  #identify points in and out of polygon	
-			  inPoly <- point.in.polygon(path.fwd$x[rows.i],path.fwd$y[rows.i],polyg$x,polyg$y)
+			  inPoly <- sp::point.in.polygon(path.fwd$x[rows.i],path.fwd$y[rows.i],
+			   polyg$x,polyg$y)
 			  
 		  } #end while
 		 
 		  #update progress bar
-			info <- sprintf("%d%% done", round(i/(nwin)*100))
-			setTxtProgressBar(pb, i, label=info)
+			setTxtProgressBar(pb, i)
 			if(i==(nwin)) close(pb)		
 		} #end i
       
