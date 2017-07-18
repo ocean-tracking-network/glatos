adf26099cc05ad7550ff3eb2302546dc54533265
Angela Dini <dinian@T941E.WPA.Dal.Ca>
2017-07-18 10:09:40 -0300

    Edited code, made abacusPlot work with OTN and GLATOS data

---
+++ R/visualization/abacusPlot.R
@@ -0,0 +1,239 @@
+#' Plot detection locations of acoustic transmitters over time
+#'
+#' Plot detection locations of acoustic transmitters over time
+#'
+#' @param detections A data frame containing at least two columns with names 
+#'   specified by \code{detColNames}. The 'location' column contains the 
+#'   locations (typically 'glatos_array' or 'station' for GLATOS data) that 
+#'   will be plotted on the y-axis. The 'timestamp' column contains the 
+#'   datetime stamps for the detections (MUST be of class 'POSIXct').
+#'   
+#' @param detColNames An optional list of character strings with names of
+#'   required columns in \code{detections}:
+#' \itemize{
+#'   \item \code{locationCol} A character scalar with the name (in quotes) of the 
+#'      column containing the location codes to be plotted on the y-axis. The 
+#'      default value ("glatos_array") is consistent with GLATOS standard.
+#'   \item \code{timestampCol} A character scalar with the name (in quotes) of the 
+#'      column containing the timestamp data to be plotted on the x-axis.
+#'      The default value ("detection_timestamp_utc") is consistent with 
+#'      GLATOS standard.
+#' }
+#'    
+#' @param controlTable Optional data frame with two columns, c('location', and 
+#'   'y_order). The 'location' column is a character vector of locations to be 
+#'   plotted on the y-axis and the name of the 'location' column must match the 
+#'   'location' column in \code{detections} data frame 
+#'   (set by \code{detColNames}). The 'y_order' column specifies what order the 
+#'   grouping variable will appear on the y-axis (y_order increases as you move 
+#'   away from the x-axis).
+#'   
+#' @param plotTitle An optional character scalar that will appear at the top of 
+#'   the plot. Default is no title.
+#'   
+#' @param Ylab A character scalar indicating the y-axis label that will appear 
+#'   on the figure (default will match \code{detColNames$locationCol}).
+#'   
+#' @param outFile An optional character scalar with the name of the png file 
+#'   created (including file extension; default = "AbacusPlot.png").
+#'   
+#' @param ... Other plotting arguments that pass to "plot" function 
+#'   (e.g., col, lwd, type).
+#'
+#' @details NAs are not allowed in any of the three required columns of 
+#'   \code{events}.
+#'
+#' @details The control table is used to control which locations will appear in 
+#' the plot and in what order they will appear. If no controlTable is 
+#' supplied, the function will plot only those locations that appear in the 
+#' \code{detections} data frame and the order of locations on the y-axis will  
+#' correspond to the order in which each location appears in the data frame.
+#'
+#' @details By default, the function does not distinguish detections from  
+#' different transmitters and will therefore plot all transmitters the same  
+#' color. If more than one fish is desired in a single plot, a vector of colors  
+#' must be passed to the function using the 'col =' argument. The color vector  
+#' must be the same length as the number of rows in the detections data frame. 
+#'
+#' @details #' Alternatively, plots for multiple individual fish can be created 
+#' by looping through and creating a separate plot on subsetted detections data.
+#' Plotting options (i.e., line width and color) can be changed using optional 
+#' graphical parameters 
+#' \url{http://www.statmethods.net/advgraphs/parameters.html} 
+#' that are passed to "segments" (see ?segments).
+#'
+#' @return A png file containing the abacus plot (default name "AbacusPlot.png")
+#' is written to the working directory.
+#'
+#' @author T. R. Binder
+#'
+#' @examples
+#' data("walleye_detections") #example data
+#' 
+#' head(walleye_detections)
+#' 
+#' #subset one transmitter
+#' walleye_detections <-
+#' 	 walleye_detections[walleye_detections$transmitter_id == 32123, ]
+#' 	
+#' #plot without control table
+#' abacusPlot(walleye_detections, controlTable=NULL, plotTitle = "TagID: 32123",
+#' 	 outFile="AbacusPlot_tag32123.png", col = "red")
+#' 	
+#' #get example control table
+#' data("walleye_controlTable") #example dataset
+#' walleye_controlTable
+#' 
+#' #plot with control table
+#' abacusPlot(walleye_detections, controlTable=walleye_controlTable, 
+#' 	 plotTitle = "TagID: 32123", outFile="AbacusPlot_tag32123_control.png", 
+#' 	 col = "red")
+#'
+#' #plot with custom y-axis label and lines connecting symbols
+#' abacusPlot(walleye_detections, controlTable=walleye_controlTable, 
+#' 	 plotTitle = "TagID: 32123", Ylab="Location (GLATOS Array)",
+#' 	 outFile="AbacusPlot_tag32123_control.png", 
+#' 	 col = "red", type="o")
+#' 
+#' @export
+
+# My changes: 
+# Entered column names to make it work with OTN data as well as GLATOS data
+
+# To test this, I used the sample data:
+# id  location  time
+# 1   TTB       2010/10/11 11:11:11
+# 2   OSC       2010/10/11 23:12:53
+# 3   PRS       2010/10/12 14:23:12
+# 4   OSC       2010/10/13 07:24:52
+# 5   OSC       2010/10/13 13:17:13
+# 6   TTB       2010/10/13 20:20:20
+# 7   PRS       2010/10/14 17:43:22
+# 8   PRS       2010/10/15 18:19:23
+# 9   TTB       2010/10/16 10:56:56
+# 10  OSC       2010/10/16 22:11:33
+#
+# which can be constructed using:
+
+# id <- 1:10
+# loc <- c("TTB", "OSC", "PRS", "OSC", "OSC", "TTB", "PRS", "PRS", "TTB", "OSC")
+# d <- c("2010/10/11 11:11:11", "2010/10/11 23:12:53", "2010/10/12 14:23:12", "2010/10/13 07:24:52", "2010/10/13 13:17:13", "2010/10/13 20:20:20", "2010/10/14 17:43:22", "2010/10/15 18:19:23", "2010/10/16 10:56:56", "2010/10/16 22:11:33")
+# d <- as.POSIXct(d, tz="UCT")
+# 
+# dataAP <- data.frame(id=id, location=loc, time=d)
+# dataAP$location <- as.character(dataAP$location)
+
+
+# To use:
+# For sample data, abacusPlot(dataAP, "sample")
+# For glatos data, abacusPlot(glatos, "GLATOS")
+# For OTN data, abacusPlot(otn, "OTN")
+
+abacusPlot <- function(detections, type, controlTable = NULL, plotTitle = "", Ylab=NA, outFile = "AbacusPlot.png", ...) {
+# abacusPlot <- function(detections, detColNames=list(locationCol="glatos_array", 
+# 	timestampCol="detection_timestamp_utc"), 
+# 	controlTable = NULL, 
+# 	plotTitle = "", Ylab=NA, outFile = "AbacusPlot.png", ...){
+# 	
+  if(type=="sample") {
+    detColNames = list(locationCol="location", timestampCol="time")
+  } else if(type=="GLATOS") {
+    detColNames=list(locationCol="glatos_array", timestampCol="detection_timestamp_utc")
+  } else if(type=="OTN"){
+    detColNames = list(locationCol="tagname", timestampCol="datecollected")
+  } else {
+    detColNames = {}
+  }
+  
+	# Check that the specified columns appear in the detections data frame
+	missingCols <- setdiff(unlist(detColNames), names(detections))
+	if (length(missingCols) > 0){
+		stop(paste0("Detections data frame is missing the following ",
+			"column(s):\n", paste0("       '",missingCols,"'", collapse="\n")), 
+			call.=FALSE)
+	}
+	
+	# Subset detections with only user-defined columns and change names
+	# this makes code more easy to understand (esp. ddply)
+	detections <- detections[,unlist(detColNames)] #subset
+	names(detections) <- c("location","timestamp")
+		
+	# Check that timestamp is of class 'POSIXct'
+	if(!('POSIXct' %in% class(detections$timestamp))){
+		stop(paste0("Column '",detColNames$timestampCol,
+			"' in the detections data frame must be of class 'POSIXct'."),
+			call.=FALSE)
+	} 
+
+	# If controlTable not supplied, create one - plotted in order locations 
+	#  appear in the detections data frame	
+	if(is.null(controlTable)){
+		controlTable <- data.frame(
+			location = unique(detections$location), 
+			y_order = 1:length(unique(detections$location)), 
+			stringsAsFactors = FALSE)
+	} else {
+    
+		# Check that the specified columns are in the control table data frame
+		missingCols <- setdiff(c(detColNames$locationCol, "y_order"), names(controlTable))
+		if (length(missingCols) > 0){
+			stop(paste0("Control table data frame is missing the following ",
+				"column(s):\n", paste0("       '",missingCols,"'", collapse="\n")), 
+				call.=FALSE)
+		}
+		
+		#change name of location column in control table
+		controlTable <- controlTable[,c(detColNames[[1]],"y_order")] #order columns
+		names(controlTable)[1] <- "location"
+	}
+	
+	#update Ylab value if NA
+	if(is.na(Ylab)) 
+	  Ylab <- detColNames$locationCol
+		
+	# Merge detections and controlTable data frames
+	# Keep only locations that appear in the controlTable data frame
+	detections <- merge(detections, controlTable, by = "location", all.y = TRUE)
+	
+	#sort by timestamp
+	detections <- detections[order(detections$timestamp),] 
+   
+	# Variable which scales the height of the y-axis depending on the number of 
+	# labels to appear. 
+	# Assumes 24 labels is the perfect spacing for height = 1000 px.
+	pngHeight <- max((nrow(controlTable)/24)*1000, 500)
+	
+	# Calculate a y-axis label offset to accommodate grouping variables with 
+	# different string lengths (e.g., "DRM" vs "DRM-001").
+	YlabOffset <- (max(nchar(detections$location))-3)/3
+	
+	# Create a png file containing the detections plot
+	png(outFile, height = pngHeight, width = 1000, pointsize = 22)
+	# Set inner and outer margins
+	par(mar = c(1,1,1.5,2), oma = c(3,4 + YlabOffset,0,0))
+		
+	# Plot detection data
+	with(detections, 
+			plot(timestamp, y_order, 
+			  xlim = range(timestamp, na.rm = TRUE), 
+				ylim = c(1,length(unique(location))), 
+				pch = 16, main = plotTitle, yaxt = "n", xaxt = "n", ylab = "", 
+				xlab = "", ...))
+		
+		# Add custom axes
+	axis(2, at = controlTable$y_order, 
+		labels = controlTable$location, las = 1)
+	xmaj <- seq(from = min(detections$timestamp, na.rm = TRUE), 
+		to = max(detections$timestamp, na.rm = TRUE), length.out = 5)
+	axis(1, at = xmaj, labels = format(xmaj, "%Y-%m-%d"), las = 1)
+		
+	# Add axes titles
+	mtext("Date", side = 1, line = 2.2, cex = 1.2)
+	mtext(Ylab, side = 2, line = 3.5 + YlabOffset, cex = 1.2)
+	dev.off()
+
+	#get output directory
+	outDir <- ifelse(dirname(outFile)==".", getwd(), dirname(outFile)) 
+	message(paste0("Output file is located in the following directory:\n", 
+		outDir))	
+}
