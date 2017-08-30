8ebdcd4d72b72b5a675feba006127bcdfc3e349b
Christopher Holbrook <cholbrook@usgs.gov>
2017-08-30 15:11:27 -0400

    added walley_detections.zip - sample data

---
+++ R/extdata.r
@@ -0,0 +1,17 @@
+#' @title Zipped GLATOS detection file from Huron Erie Corridor Walleye project
+#' 
+#' @description An example detection file
+#' 
+#' @format A zipped walleye detection file in detection file format 1.3:
+##' 
+#' @name raw_walleye_detections
+#' 
+#' @section Filename: walleye_detections.zip
+#' 
+#' @usage 
+#' system.file("extdata", "walleye_detections.zip", package="glatos")
+#'
+#' @author Todd Hayden
+#' 
+#' @source \url{http://glatos.glos.us/home/project/HECWL}
+NULL
