9dfa44764694c66f2c94a1629cef51967e3ff4c9
Christopher Holbrook <cholbrook@usgs.gov>
2017-09-25 16:26:19 -0400

    clean up format and make GLATOS default data type

--- R/filt-getMinLag.r
+++ R/filt-getMinLag.r
@@ -1,64 +1,71 @@
-#' Calculate 'min_lag' column of a dataset, add this column into the dataset,
-#'   and return the dataset with the new column
-#'
-#' @param detections A data frame containing detection data with at least 
-#'   3 columns containing 'transmitter', 'receiver', and 'timestamp'. 
-#'   Column names are specified by \code{type}.
-#'   
-#' @param type A character string that contains the type of data that is being passed in,
-#'   for example, "OTN", "GLATOS", or "sample".
+#' Calculate 'min_lag' for identifying potential false positive detections
 #' 
-#' @param detColNames An optional list that contains the user-defined column
+#' Calculate 'min_lag' column of a dataset, add this column into the dataset, 
+#' and return the dataset with the new column
+#' 
+#' @param detections A data frame containing detection data with at least 3
+#'   columns containing 'transmitter', 'receiver', and 'timestamp'. Column names
+#'   are specified by \code{type}.
+#'   
+#' @param type A character string that contains the type of data that is being
+#'   passed in, for example, "OTN", "GLATOS", or "sample". Default is "GLATOS".
+#'   
+#' @param detColNames An optional list that contains the user-defined column 
 #'   names
 #'   
-#' @details detColNames is defined as a list with the names of the required columns in
-#' \code{detections}, defined by \code{type}:
-#'  \itemize{
-#'    \item \code{transmitters} is a character string with the name of the column
-#'     containing the ids of the transmitters
-#'     ('transmission_id' for GLATOS data, 'tagname' for OTN data, or 'transmitter'
-#'                                                 for sample data).
-#'     \item \code{receivers} is a character string with the name of the column
-#'     containing the ids of the receivers
-#'     ('receiver_sn' for GLATOS data, 'receiver_group' for OTN data, or 'receiver'
-#'                                                 for sample data).
-#'     \item \code{timestampCol} is a character string with the name of the column
-#'     containing datetime stamps for the detections (MUST be of class 'POSIXct')
-#'     ('detection_timestamp_utc' for GLATOS data, 'datecollected' for OTN data, or
-#'                                                 'time' for sample data).
-#'   }
-#'
-#' @details Each value in the min_lag column defines the minimum number of seconds
-#'  between either a time with the same transmitter and receiver if before or after
-#'  the current time. This is calculated with the dplyr package in R. This min_lag 
-#'  column will be used in falseDetectionFilter.R to filter false data.
-#'  @details 
-#' A new column (\code{min_lag}), indicating the minimum lag of each entry (according
-#' to the definition above), is added to the input data frame.
+#' @details detColNames is defined as a list with the names of the required
+#'   columns in \code{detections}, defined by \code{type}: \itemize{ \item
+#'   \code{transmitters} is a character string with the name of the column 
+#'   containing the ids of the transmitters ('transmission_id' for GLATOS data,
+#'   'tagname' for OTN data, or 'transmitter' for sample data). \item
+#'   \code{receivers} is a character string with the name of the column 
+#'   containing the ids of the receivers ('receiver_sn' for GLATOS data,
+#'   'receiver_group' for OTN data, or 'receiver' for sample data). \item
+#'   \code{timestampCol} is a character string with the name of the column 
+#'   containing datetime stamps for the detections (MUST be of class 'POSIXct') 
+#'   ('detection_timestamp_utc' for GLATOS data, 'datecollected' for OTN data,
+#'   or 'time' for sample data). }
+#'   
+#' @details Each value in the min_lag column defines the minimum number of
+#'   seconds between either a time with the same transmitter and receiver if
+#'   before or after the current time. This is calculated with the dplyr package
+#'   in R. This min_lag column will be used in falseDetectionFilter.R to filter
+#'   false data.
+#'   
+#' @details A new column (\code{min_lag}), indicating the minimum lag of each
+#' entry (according to the definition above), is added to the input data frame.
 #' 
 #' @return A data frame containing the data with a min_lag column appended
-#'
+#'   
 #' @author A. Dini
-#'
-#' @usage To use:
-#'   For GLATOS data, getMinLag(data, "GLATOS")
-#'   For OTN data, getMinLag(data, "OTN")
-#'   For sample data, getMinLag(data, "sample")
-#'
+#'   
+#' @examples
+#' library(glatos)
+#' 
+#' #Example with GLATOS data
+#' data(walleye_detections)
+#' 
+#' dtc2 <- getMinLag(walleye_detections)
+#'   
 #' @export
 
-getMinLag <- function(detections, type, detColNames=list()) {
+getMinLag <- function(detections, type = "GLATOS", detColNames = list()) {
   #Check if user has not set column names
   if(length(detColNames) == 0) {
     if(type == "GLATOS") { #Set column names for GLATOS data
-      detColNames <- list(transmittersCol = "transmitter_id", receiversCol = "receiver_sn", timestampCol = "detection_timestamp_utc")
-      detections$minLag <- detections$min_lag
+      detColNames <- list(transmittersCol = "transmitter_id", 
+                          receiversCol = "receiver_sn", 
+                          timestampCol = "detection_timestamp_utc")
     } else if (type == "OTN"){ #Set column names for OTN data
-      detColNames <- list(transmittersCol = "tagname", receiversCol = "receiver_group", timestampCol = "datecollected")
+      detColNames <- list(transmittersCol = "tagname", 
+                          receiversCol = "receiver_group", 
+                          timestampCol = "datecollected")
     } else if (type == "sample") { #Set column names for sample data
-      detColNames <- list(transmittersCol = "transmitter", receiversCol = "receiver", timestampCol = "time")
+      detColNames <- list(transmittersCol = "transmitter", 
+                          receiversCol = "receiver", 
+                          timestampCol = "time")
     } else { #Other type
-      stop(paste0("The type '",type,"' is not defined."), call.=FALSE)
+      stop(paste0("The type '", type, "' is not defined."), call. = FALSE)
     }
   }
   
@@ -66,28 +73,31 @@ getMinLag <- function(detections, type, detColNames=list()) {
   missingCols <- setdiff(unlist(detColNames), names(detections))
   if (length(missingCols) > 0){
     stop(paste0("Detections dataframe is missing the following ",
-                "column(s):\n", paste0("       '",missingCols,"'", collapse="\n")), 
-         call.=FALSE)
+                "column(s):\n", paste0("       '", missingCols, "'", 
+                collapse="\n")), 
+         call. = FALSE)
   }
   
   # Subset detections with only user-defined columns and change names
   # This makes code easier to understand
-  detections2 <- detections[,unlist(detColNames)] #subset
+  detections2 <- detections[ , unlist(detColNames)] #subset
   names(detections2) <- c("transmitters","receivers","timestamp")
   detections2$num <- as.numeric(detections2$timestamp) #Add another column
   
   #Check that timestamp is of class 'POSIXct'
   if(!('POSIXct' %in% class(detections2$timestamp))){
-    stop(paste0("Column '",detColNames$timestampCol,
+    stop(paste0("Column '", detColNames$timestampCol,
                 "' in the detections dataframe must be of class 'POSIXct'."),
-         call.=FALSE)
+         call. = FALSE)
   }
   
   #Split detections by transmitter id and receiver
-  li <- split(detections2, list(detections2$transmitters, detections2$receivers))
+  li <- split(detections2, 
+              list(detections2$transmitters, detections2$receivers))
   
   #Create empty list for minimum lag
   mL <<- {} # <<- makes mL a global variable
+  
   #Remove empty data frames from the list
   li <- li[sapply(li, function(x) dim(x)[1]) > 0]
   
