acf3d3adc55dd0c77fa971a085331b9a86ac33d5
Alex Nunes <alex.et.nunes@gmail.com>
2017-11-02 15:10:30 -0300

    Changes to be committed: 	modified:   R/filt-falseDetectionFilter.r 	modified:   R/filt-getMinLag.r
    Fix for minLag using pincock algorithm

--- R/filt-falseDetectionFilter.r
+++ R/filt-falseDetectionFilter.r
@@ -2,89 +2,89 @@
 #'
 #' Identify possible false detections based on "short interval" criteria
 #'
-#' @param detections A data frame containing detection data (e.g., from the 
-#'   standard GLATOS detection export file '*_detectionsWithLocs.csv'). 
-#'   Must contain the column \code{min_lag} or equivalent (specified by 
-#'   \code{minLagCol}). \code{min_lag} is loosely based on the the 
-#'   "short interval" method described by Pincock (2012). In this case 
-#'   (GLATOS), it is defined for each detection as the shortest interval 
-#'   (in seconds) between either the previous or next detection (whichever 
+#' @param detections A data frame containing detection data (e.g., from the
+#'   standard GLATOS detection export file '*_detectionsWithLocs.csv').
+#'   Must contain the column \code{min_lag} or equivalent (specified by
+#'   \code{minLagCol}). \code{min_lag} is loosely based on the the
+#'   "short interval" method described by Pincock (2012). In this case
+#'   (GLATOS), it is defined for each detection as the shortest interval
+#'   (in seconds) between either the previous or next detection (whichever
 #'   is closest) of the same transmitter on the same receiver.
-#'   
-#' @param type A character string that contains the type of data that is being 
+#'
+#' @param type A character string that contains the type of data that is being
 #'   passed in, for example, "OTN", "GLATOS", or "sample". Default is "GLATOS".
-#'   
-#' @param tf A numeric scalar indicating the time threshold (in seconds; e.g., 
-#'   Pincock's (2012) "short interval") for identifying possible false 
+#'
+#' @param tf A numeric scalar indicating the time threshold (in seconds; e.g.,
+#'   Pincock's (2012) "short interval") for identifying possible false
 #'   detections.
-#'   
-#' @param minLagCol A character string containing the name of the column 
+#'
+#' @param minLagCol A character string containing the name of the column
 #'   in \code{detections} that contains 'min_lag'.
-#' 
+#'
 #' @param detColNames An optional list that contains the user-defined column
 #'   names
-#'   
-#' @details The min_lag column should be in \code{detections} but if not, 
+#'
+#' @details The min_lag column should be in \code{detections} but if not,
 #'  program will add in using getMinLag() function.
-#'   
-#' @details Detections are identified as potentially false when 
+#'
+#' @details Detections are identified as potentially false when
 #'   \code{min_lag > tf}.
-#'   
-#' @details 
-#' A new column (\code{passedFilter}), indicating if each record (row) passed 
-#' the filter, is added to the input data frame. 
-#' 
-#' @details  
-#' A common rule of thumb for choosing tf for VEMCO PPM encoded transmitters 
-#'   is 30 times the nominal delay (e.g., 3600 s for a transmitter with a 
+#'
+#' @details
+#' A new column (\code{passedFilter}), indicating if each record (row) passed
+#' the filter, is added to the input data frame.
+#'
+#' @details
+#' A common rule of thumb for choosing tf for VEMCO PPM encoded transmitters
+#'   is 30 times the nominal delay (e.g., 3600 s for a transmitter with a
 #'   120 s nominal delay) - see Pincock (2012).
 #'
-#' @return A data frame consisting of \code{detections} with an additional 
-#'   column 'passedFilter' indicating if each detection did (1) or did not (0) 
+#' @return A data frame consisting of \code{detections} with an additional
+#'   column 'passedFilter' indicating if each detection did (1) or did not (0)
 #'   pass the criteria.
 #'
 #' @author T. R. Binder, edited by A. Dini
 #'
 #' @references
-#'   Pincock, D.G., 2012. False detections: what they are and how to remove them 
-#'     from detection data. Vemco Division, Amirix Systems Inc., Halifax, 
+#'   Pincock, D.G., 2012. False detections: what they are and how to remove them
+#'     from detection data. Vemco Division, Amirix Systems Inc., Halifax,
 #'     Nova Scotia.
 #'     \cr \url{http://www.vemco.com/pdf/false_detections.pdf}
 #' @references
-#'   Simpfendorfer, C.A., Huveneers, C., Steckenreuter, A., Tattersall, K., 
-#'     Hoenner, X., Harcourt, R. and Heupel, M.R., 2015. Ghosts in the data: 
-#'     false detections in VEMCO pulse position modulation acoustic telemetry 
+#'   Simpfendorfer, C.A., Huveneers, C., Steckenreuter, A., Tattersall, K.,
+#'     Hoenner, X., Harcourt, R. and Heupel, M.R., 2015. Ghosts in the data:
+#'     false detections in VEMCO pulse position modulation acoustic telemetry
 #'     monitoring equipment. Animal Biotelemetry, 3(1), p.55.
 #'     \cr \url{https://animalbiotelemetry.biomedcentral.com/articles/10.1186/s40317-015-0094-z}
-#' 
+#'
 #' @examples
 #' library(glatos)
-#' 
+#'
 #' # example - GLATOS data
 #' data("walleye_detections") #example data
-#' 
+#'
 #' head(walleye_detections)
 #'
 #' dtx <- falseDetectionFilter(walleye_detections, 3600)
 #' head(dtx)
-#'   
+#'
 #' @export
 
-falseDetectionFilter <- function(detections, type = "GLATOS", tf = 3600, 
+falseDetectionFilter <- function(detections, type = "GLATOS", tf = 3600,
   minLagCol = "min_lag", detColNames=list()){
-  
+
   # Check that the minLag column is in the detections dataframe
   # If not, add it in using getMinLag method
   if (!(minLagCol %in% names(detections))){
     detections <- getMinLag(detections, type, detColNames) #Get min_lag column
   }
-  
-  # Identify possible false detections by comparing "min_lag" column to 
+
+  # Identify possible false detections by comparing "min_lag" column to
   #  threshold defined in object "tf".
   detections$passedFilter <- ifelse(!is.na(detections[,minLagCol]) & 
                                       detections[,minLagCol] <= tf, 1, 0)
   nr <- nrow(detections)
-  
+
   message(paste0("The filter identified ",
                  nr - sum(detections$passedFilter), " (",
                  round((nr - sum(detections$passedFilter))/
--- R/filt-getMinLag.r
+++ R/filt-getMinLag.r
@@ -1,132 +1,106 @@
 #' Calculate 'min_lag' for identifying potential false positive detections
-#' 
-#' Calculate 'min_lag' column of a dataset, add this column into the dataset, 
+#'
+#' Calculate 'min_lag' column of a dataset, add this column into the dataset,
 #' and return the dataset with the new column
-#' 
+#'
 #' @param detections A data frame containing detection data with at least 3
 #'   columns containing 'transmitter', 'receiver', and 'timestamp'. Column names
 #'   are specified by \code{type}.
-#'   
+#'
 #' @param type A character string that contains the type of data that is being
 #'   passed in, for example, "OTN", "GLATOS", or "sample". Default is "GLATOS".
-#'   
-#' @param detColNames An optional list that contains the user-defined column 
+#'
+#' @param detColNames An optional list that contains the user-defined column
 #'   names
-#'   
+#'
 #' @details detColNames is defined as a list with the names of the required
 #'   columns in \code{detections}, defined by \code{type}: \itemize{ \item
-#'   \code{transmitters} is a character string with the name of the column 
+#'   \code{transmitters} is a character string with the name of the column
 #'   containing the ids of the transmitters ('transmission_id' for GLATOS data,
 #'   'tagname' for OTN data, or 'transmitter' for sample data). \item
-#'   \code{receivers} is a character string with the name of the column 
+#'   \code{receivers} is a character string with the name of the column
 #'   containing the ids of the receivers ('receiver_sn' for GLATOS data,
 #'   'receiver_group' for OTN data, or 'receiver' for sample data). \item
-#'   \code{timestampCol} is a character string with the name of the column 
-#'   containing datetime stamps for the detections (MUST be of class 'POSIXct') 
+#'   \code{timestampCol} is a character string with the name of the column
+#'   containing datetime stamps for the detections (MUST be of class 'POSIXct')
 #'   ('detection_timestamp_utc' for GLATOS data, 'datecollected' for OTN data,
 #'   or 'time' for sample data). }
-#'   
+#'
 #' @details Each value in the min_lag column defines the minimum number of
 #'   seconds between either a time with the same transmitter and receiver if
 #'   before or after the current time. This is calculated with the dplyr package
 #'   in R. This min_lag column will be used in falseDetectionFilter.R to filter
 #'   false data.
-#'   
+#'
 #' @details A new column (\code{min_lag}), indicating the minimum lag of each
 #' entry (according to the definition above), is added to the input data frame.
-#' 
+#'
 #' @return A data frame containing the data with a min_lag column appended
-#'   
+#'
 #' @author A. Dini
-#'   
+#'
 #' @examples
 #' library(glatos)
-#' 
+#'
 #' #Example with GLATOS data
 #' data(walleye_detections)
-#' 
+#'
 #' dtc2 <- getMinLag(walleye_detections)
-#'   
+#'
 #' @export
-
+require(lazyeval)
 getMinLag <- function(detections, type = "GLATOS", detColNames = list()) {
   #Check if user has not set column names
   if(length(detColNames) == 0) {
     if(type == "GLATOS") { #Set column names for GLATOS data
-      detColNames <- list(transmittersCol = "transmitter_id", 
-                          receiversCol = "receiver_sn", 
+      detColNames <- list(transmittersCol = "transmitter_id",
+                          receiversCol = "receiver_sn",
                           timestampCol = "detection_timestamp_utc")
     } else if (type == "OTN"){ #Set column names for OTN data
-      detColNames <- list(transmittersCol = "tagname", 
-                          receiversCol = "receiver_group", 
+      detColNames <- list(transmittersCol = "catalognumber",
+                          receiversCol = "station",
                           timestampCol = "datecollected")
     } else if (type == "sample") { #Set column names for sample data
-      detColNames <- list(transmittersCol = "transmitter", 
-                          receiversCol = "receiver", 
+      detColNames <- list(transmittersCol = "transmitter",
+                          receiversCol = "receiver",
                           timestampCol = "time")
     } else { #Other type
       stop(paste0("The type '", type, "' is not defined."), call. = FALSE)
     }
   }
-  
+
   # Check that the specified columns above appear in the detections dataframe
   missingCols <- setdiff(unlist(detColNames), names(detections))
+
   if (length(missingCols) > 0){
     stop(paste0("Detections dataframe is missing the following ",
-                "column(s):\n", paste0("       '", missingCols, "'", 
-                collapse="\n")), 
+                "column(s):\n", paste0("       '", missingCols, "'",
+                collapse="\n")),
          call. = FALSE)
   }
-  
-  # Subset detections with only user-defined columns and change names
-  # This makes code easier to understand
-  detections2 <- detections[ , unlist(detColNames)] #subset
-  names(detections2) <- c("transmitters","receivers","timestamp")
-  detections2$num <- as.numeric(detections2$timestamp) #Add another column
-  
-  #Check that timestamp is of class 'POSIXct'
-  if(!('POSIXct' %in% class(detections2$timestamp))){
-    stop(paste0("Column '", detColNames$timestampCol,
-                "' in the detections dataframe must be of class 'POSIXct'."),
-         call. = FALSE)
-  }
-  
-  #Split detections by transmitter id and receiver
-  li <- split(detections2, 
-              list(detections2$transmitters, detections2$receivers))
-  
-  #Create empty list for minimum lag
-  mL <<- {} # <<- makes mL a global variable
-  
-  #Remove empty data frames from the list
-  li <- li[sapply(li, function(x) dim(x)[1]) > 0]
-  
-  #Calculate each minimum lag of each row for each data frame in the list
-  list2 <- lapply(li, function(x) {
-    if(nrow(x)==1) {
-      #Set data frames in list with 1 entry to have a minimum lag of NA
-      minLag2 <- NA
-    } else {
-      n <- as.numeric(x$timestamp)
-      #Calculate time between current and previous entry for each entry
-      mL1a <- (n-dplyr::lag(n))
-      #Calculate time between current and next entry for each entry (using dplyr)
-      mL1b <- (dplyr::lead(n)-n)
-      #Combine into a data frame
-      d <- data.frame(d1=mL1a, d2=mL1b)
-      #Calculate minimum of the two for each row, ignoring the NA entries
-      minLag2 <- apply(d,1,function(x) min(x, na.rm = TRUE))
-    }
-    #Append answer to mL list
-    mL <<- append(mL, minLag2)
-  })
-  #Combine list of data frames
-  detections2 <- do.call("rbind", li)
-  #Add min lag column to detections data frame
-  detections2$mLag <- mL
-  detections2 <- detections2[order(detections2$num),] #Put them back in the original order to be able to append the answers to detections
-  detections$min_lag <- detections2$mLag
-  
-  #Return original dataset with "min_lag" column attached to it
-  return(detections)
+
+
+  detections <- detections %>% mutate_(timestamp = detColNames$timestampCol)
+  detections <- mutate(detections, timestamp = as.POSIXct(detections$timestamp))
+  detections <- mutate(detections, this.date = as.character(detections$timestamp))
+
+  out <- detections %>%
+  group_by_(c(detColNames$transmittersCol, detColNames$receiversCol)) %>%
+
+ # Arrange these groups in order of animal and timestamp
+ arrange_(detColNames$transmittersCol, detColNames$timestampCol) %>%
+
+ mutate_( # lag() and lead() are a snappy way to peek ahead and behind each row,
+     # ifelse so we only look at the other rows on station/animal match. Others get NA
+   last.date = interp(~ifelse(tc == lag(tc) & rc == lag(rc), lag(this.date), NA), rc=as.name(detColNames$receiversCol), tc=as.name(detColNames$transmittersCol)),
+   next.date = interp(~ifelse(tc == lead(tc) & rc == lead(rc), lead(this.date), NA), rc=as.name(detColNames$receiversCol), tc=as.name(detColNames$transmittersCol))
+ ) %>%
+ mutate( # Less-snappy way to run a bunch of conditional logic on what's ahead and behind.
+       last.diff = ifelse(is.na(last.date), NA, difftime(this.date, last.date, units="secs")),
+       next.diff = ifelse(is.na(next.date), NA, difftime(next.date, this.date, units="secs"))
+     ) %>% # Now can run the filter step to evaluate
+   mutate(
+    min_lag = pmin(last.diff, next.diff, na.rm=TRUE),
+    )
+     return(out)
 }
