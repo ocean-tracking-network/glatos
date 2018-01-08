b0cff365df719551bb7a0d90b14e96761360c46d
Todd Hayden <thayden@usgs.gov>
2018-01-08 13:12:53 -0500

    merged load file functions with my work to fix up animation functions.  glatos library installs but has some loose ends in animatePath.r function

--- R/animatePath.r
+++ R/animatePath.r
@@ -130,150 +130,149 @@
 #' @export
 
 
-library(glatos)
-# create procdata for development
-# example detection data
-## data(walleye_detections) 
-## dtc <- walleye_detections
-## dtc <- dtc[, c("animal_id", "detection_timestamp_utc", "deploy_lat", "deploy_long")]
-## data(greatLakesTrLayer)
-## trans <- greatLakesTrLayer
-## procObj <- interpolatePath(dtc, trans=greatLakesTrLayer)
-## saveRDS(procObj, "procObj.rds")
+animatePath <- function(x,y,z){
+## library(glatos)
+## # create procdata for development
+## # example detection data
+## ## data(walleye_detections) 
+## ## dtc <- walleye_detections
+## ## dtc <- dtc[, c("animal_id", "detection_timestamp_utc", "deploy_lat", "deploy_long")]
+## ## data(greatLakesTrLayer)
+## ## trans <- greatLakesTrLayer
+## ## procObj <- interpolatePath(dtc, trans=greatLakesTrLayer)
+## ## saveRDS(procObj, "procObj.rds")
 
-#development
-procObj <- readRDS("procObj.rds")
-# example receiver location data
-data(recLoc_example) 
-data(greatLakesPoly) 
-background <- greatLakesPoly
-backgroundYlim = c(41.48, 45.90)
-backgroundXlim = c(-84.0, -79.5)
-recs <- recLoc_example
-int_time_stamp <- 86400
-outDir <- "~/Desktop/test"
+## #development
+## procObj <- readRDS("procObj.rds")
+## # example receiver location data
+## data(recLoc_example) 
+## data(greatLakesPoly) 
+## background <- greatLakesPoly
+## backgroundYlim = c(41.48, 45.90)
+## backgroundXlim = c(-84.0, -79.5)
+## recs <- recLoc_example
+## int_time_stamp <- 86400
+## outDir <- "~/Desktop/test"
 
-plot_control <- data.frame(id = c(3, 10, 22, 23, 153, 167, 171, 234, 444, 479, 3, 10, 22, 23, 153, 167, 171, 234, 444, 479), position_type = c("real", "real", "real", "real", "real", "real", "real", "real", "real", "real", "inter", "inter", "inter", "inter", "inter", "inter", "inter", "inter", "inter", "inter"), color = c("pink", "pink", "pink", "pink", "pink", "pink", "pink", "pink", "pink", "pink", "red", "red", "red", "red", "red", "red", "red", "red", "red", "red"), marker = c(21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21), marker_cex = rep(1,20), line_color = NA, line_width = NA )
+## plot_control <- data.frame(id = c(3, 10, 22, 23, 153, 167, 171, 234, 444, 479, 3, 10, 22, 23, 153, 167, 171, 234, 444, 479), position_type = c("real", "real", "real", "real", "real", "real", "real", "real", "real", "real", "inter", "inter", "inter", "inter", "inter", "inter", "inter", "inter", "inter", "inter"), color = c("pink", "pink", "pink", "pink", "pink", "pink", "pink", "pink", "pink", "pink", "red", "red", "red", "red", "red", "red", "red", "red", "red", "red"), marker = c(21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21), marker_cex = rep(1,20), line_color = NA, line_width = NA )
 
-# need to add "tail_dur = 0" back in...
-# animatepath (this needs tested...)
-animatePath <- function(procObj, recs, outDir, background = NULL,
-                        backgroundYlim = c(41.48, 45.9),
-                        backgroundXlim = c(-84, -79.5),
-                        ffmpeg = NA, plotControl = NULL,
-                        int_time_stamp = 86400,
-                        ani_out = "animation.mp4", frame_delete = FALSE )
+## # need to add "tail_dur = 0" back in...
+## # animatepath (this needs tested...)
+## animatePath <- function(procObj, recs, outDir, background = NULL,
+##                         backgroundYlim = c(41.48, 45.9),
+##                         backgroundXlim = c(-84, -79.5),
+##                         ffmpeg = NA, plotControl = NULL,
+##                         int_time_stamp = 86400,
+##                         ani_out = "animation.mp4", frame_delete = FALSE )
 
-setDT(procObj)
-setDT(recs)
+## setDT(procObj)
+## setDT(recs)
 
-# try calling ffmpeg
-# add exe if ffmpeg is directory
-cmd <- ifelse(grepl("ffmpeg.exe$",ffmpeg) | is.na(ffmpeg),
-              ffmpeg, paste0(ffmpeg,"\\ffmpeg.exe"))
-cmd <- ifelse(is.na(ffmpeg), 'ffmpeg', cmd)	
-ffVers <- suppressWarnings(system2(cmd, "-version",stdout=F)) #call ffmpeg
-if(ffVers == 127) stop(paste0('"ffmpeg.exe" was not found.\n',
-'Ensure it is installed add added to system PATH variable\n',
-"or specify path using input argument 'ffmpeg'\n\n",
-'FFmpeg is available from:\n https://ffmpeg.org/'), call.=FALSE)
+## # try calling ffmpeg
+## # add exe if ffmpeg is directory
+## cmd <- ifelse(grepl("ffmpeg.exe$",ffmpeg) | is.na(ffmpeg),
+##               ffmpeg, paste0(ffmpeg,"\\ffmpeg.exe"))
+## cmd <- ifelse(is.na(ffmpeg), 'ffmpeg', cmd)	
+## ffVers <- suppressWarnings(system2(cmd, "-version",stdout=F)) #call ffmpeg
+## if(ffVers == 127) stop(paste0('"ffmpeg.exe" was not found.\n',
+## 'Ensure it is installed add added to system PATH variable\n',
+## "or specify path using input argument 'ffmpeg'\n\n",
+## 'FFmpeg is available from:\n https://ffmpeg.org/'), call.=FALSE)
 
-  # add colors and symbols to detections data frame
-  if(!is.null(plotControl)){	
-  # merge plotControl with procObj
-  setDT(plot_control)
-    procObj <- merge(procObj, plot_control, by.x=c("animal_id", "type"),
-                     by.y=c("id","position_type"))
-          } else {
-      #otherwise, assign default colors and symbols
-       procObj$color = 'black'
-       procObj$marker = 21
-       procObj$marker_cex = 1
-       procObj$line_color = NA
-       procObj$line_width = NA
-           }		
+##   # add colors and symbols to detections data frame
+##   if(!is.null(plotControl)){	
+##   # merge plotControl with procObj
+##   setDT(plot_control)
+##     procObj <- merge(procObj, plot_control, by.x=c("animal_id", "type"),
+##                      by.y=c("id","position_type"))
+##           } else {
+##       #otherwise, assign default colors and symbols
+##        procObj$color = 'black'
+##        procObj$marker = 21
+##        procObj$marker_cex = 1
+##        procObj$line_color = NA
+##        procObj$line_width = NA
+##            }		
 		
-#make output directory if it does not already exist
-if(!dir.exists(outDir))	dir.create(outDir)
+## #make output directory if it does not already exist
+## if(!dir.exists(outDir))	dir.create(outDir)
 
-# create sequence of timestamps based on min/max timestamps in data
-rng <- as.POSIXct(trunc(range(procObj$bin_stamp), units = "days"),
-                  tz = "GMT")
-t_seq <- seq(rng[1], rng[2], int_time_stamp)
+## # create sequence of timestamps based on min/max timestamps in data
+## rng <- as.POSIXct(trunc(range(procObj$bin_stamp), units = "days"),
+##                   tz = "GMT")
+## t_seq <- seq(rng[1], rng[2], int_time_stamp)
 
-# add bins to processed object for plotting
-procObj[, plot_bin := findInterval(bin_stamp, t_seq)]
+## # add bins to processed object for plotting
+## procObj[, plot_bin := findInterval(bin_stamp, t_seq)]
 
-# create group identifier
-procObj[, grp := plot_bin]
+## # create group identifier
+## procObj[, grp := plot_bin]
 
-# remove receivers not recovered (records with NA in recover_date_time)
-setkey(recs, recover_date_time)
-recs <- recs[!J(NA_real_), c("station", "deploy_lat", "deploy_long",
-                             "deploy_date_time", "recover_date_time")]
+## # remove receivers not recovered (records with NA in recover_date_time)
+## setkey(recs, recover_date_time)
+## recs <- recs[!J(NA_real_), c("station", "deploy_lat", "deploy_long",
+##                              "deploy_date_time", "recover_date_time")]
 
-# bin data by time interval and add to recs
-recs[, start := findInterval(deploy_date_time, t_seq)]
-recs[, end := findInterval(recover_date_time, t_seq)]
+## # bin data by time interval and add to recs
+## recs[, start := findInterval(deploy_date_time, t_seq)]
+## recs[, end := findInterval(recover_date_time, t_seq)]
 
-# add clock for plot
-procObj[, clk := t_seq[plot_bin]]
+## # add clock for plot
+## procObj[, clk := t_seq[plot_bin]]
 
-# determine leading zeros needed by ffmpeg and add as new column
-char <- paste0("%", 0, nchar(as.character(max(procObj$plot_bin))), "d")
-procObj[, f_name := paste0(sprintf(char, plot_bin), ".png")]
+## # determine leading zeros needed by ffmpeg and add as new column
+## char <- paste0("%", 0, nchar(as.character(max(procObj$plot_bin))), "d")
+## procObj[, f_name := paste0(sprintf(char, plot_bin), ".png")]
 
-cust_plot <- function(x){
+## cust_plot <- function(x){
 
-  # extract receivers in the water during plot interval
-  sub_recs <- recs[between(x$plot_bin[1], lower = recs$start, upper = recs$end)]
+##   # extract receivers in the water during plot interval
+##   sub_recs <- recs[between(x$plot_bin[1], lower = recs$start, upper = recs$end)]
 
-  # plot GL outline and movement points
-  png(file.path(outDir, x$f_name[1]), width = 3200, height = 2400,
-      units = 'px', res = 300)
+##   # plot GL outline and movement points
+##   png(file.path(outDir, x$f_name[1]), width = 3200, height = 2400,
+##       units = 'px', res = 300)
 
-  # plot background image
-  par(oma=c(0,0,0,0), mar=c(0,0,0,0))  #no margins
-  if(is.null(background)) {
-    data(greatLakesPoly) #example in glatos package
-    background <- greatLakesPoly
-  }
+##   # plot background image
+##   par(oma=c(0,0,0,0), mar=c(0,0,0,0))  #no margins
+##   if(is.null(background)) {
+##     data(greatLakesPoly) #example in glatos package
+##     background <- greatLakesPoly
+##   }
   
-  #note call to plot with sp
-  sp::plot(background, ylim = c(backgroundYlim), xlim = c(backgroundXlim),
-           axes = FALSE, lwd = 2)
+##   #note call to plot with sp
+##   sp::plot(background, ylim = c(backgroundYlim), xlim = c(backgroundXlim),
+##            axes = FALSE, lwd = 2)
 
-  # plot fish locations, receivers, clock  
-  points(x = sub_recs$deploy_long, y = sub_recs$deploy_lat, pch = 21, cex = 2,
-         col = 'tan2', bg = 'tan2')
-  text(x = -84.0, y = 42.5, as.Date(x$clk[1]), cex = 2.5)
-  points(x = x$i_lon, y = x$i_lat, pch = x$marker, col = x$color,
-         cex = x$marker_cex)
-  dev.off()
+##   # plot fish locations, receivers, clock  
+##   points(x = sub_recs$deploy_long, y = sub_recs$deploy_lat, pch = 21, cex = 2,
+##          col = 'tan2', bg = 'tan2')
+##   text(x = -84.0, y = 42.5, as.Date(x$clk[1]), cex = 2.5)
+##   points(x = x$i_lon, y = x$i_lat, pch = x$marker, col = x$color,
+##          cex = x$marker_cex)
+##   dev.off()
+## }
+
+## # create images
+## procObj[,  cust_plot(x = .SD),  by = grp,
+##         .SDcols = c("plot_bin", "clk", "i_lon", "i_lat", "marker", "color",
+##                     "marker_cex", "f_name")]
+
+## if(ani_out = NULL){stop}
+
+## # need to install mapmate from github using "install.git"
+## # Need to decide how to handle supplying arguments to this.
+## # One option is to make it an additional function.
+## # Another is to create a list that feeds into ffmpeg function.
+## # Need to facilitate simple manipulations of animation
+## # (i.e., speed up/slow down, different output formats, etc.)   
+## mapmate::ffmpeg(dir = outDir, pattern = paste0(char, ".png"), output = ani_out,
+##                 output_dir = outDir, rate = "ntsc")  
+
+## # if frame_delete = TRUE, then delete frames after making animation.
+## if(frame_delete = TRUE){
+##   unlink(file.path(outDir, unique(procObj$f_name)))
+##   }
+## }
+
 }
-
-# create images
-procObj[,  cust_plot(x = .SD),  by = grp,
-        .SDcols = c("plot_bin", "clk", "i_lon", "i_lat", "marker", "color",
-                    "marker_cex", "f_name")]
-
-if(ani_out = NULL){stop}
-
-# need to install mapmate from github using "install.git"
-# Need to decide how to handle supplying arguments to this.
-# One option is to make it an additional function.
-# Another is to create a list that feeds into ffmpeg function.
-# Need to facilitate simple manipulations of animation
-# (i.e., speed up/slow down, different output formats, etc.)   
-mapmate::ffmpeg(dir = outDir, pattern = paste0(char, ".png"), output = ani_out,
-                output_dir = outDir, rate = "ntsc")  
-
-# if frame_delete = TRUE, then delete frames after making animation.
-if(frame_delete = TRUE){
-  unlink(file.path(outDir, unique(procObj$f_name)))
-  }
-}
-
-
-
-
