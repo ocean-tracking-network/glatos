35413a1c01b232cce85831324f5bb9bdc58d143e
Christopher Holbrook <cholbrook@usgs.gov>
2018-02-13 16:16:32 -0500

    add internal functions for managing internal datasets

---
+++ R/utils-intern_pkg_man.r
@@ -0,0 +1,19 @@
+#Interal utility functions for package management
+
+#--------------------------------------------------------------------------
+#Add an internal data object to sysdata.rda
+add_internal_data <- function(x, rda_file) {
+  e <- new.env()
+  load(rda_file, e) #load existing internals to new envir
+  assign(deparse(substitute(x)), x, envir = e) #add/replace with new object
+  save(list = ls(e), file = rda_file, envir = e)
+}
+
+#--------------------------------------------------------------------------
+#Remove an internal data object from sysdata.rda
+remove_internal_data <- function(x, rda_file) {
+  e <- new.env()
+  load(rda_file, e) #load existing internals to new envir
+  rm(list = deparse(substitute(x)), envir = e) #remove unwanted object
+  save(list = ls(e), file = rda_file, envir = e)
+}
