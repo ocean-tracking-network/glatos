521715145a46b7f7ac7463308db5255d9e3e007f
Christopher Holbrook <cholbrook@usgs.gov>
2018-02-14 20:41:41 -0500

    replace lamprey detections dataset; fix bad glatos_caught_date and add example with depth and tag recovery

--- tests/testthat/test-read_glatos_detections.r
+++ tests/testthat/test-read_glatos_detections.r
@@ -4,13 +4,25 @@ context("Check read_glatos_detections")
 
 # Actual result
 #get path to example detection file
-det_file <- system.file("extdata", 
-  "walleye_detections.csv",package="glatos")
+wd_file <- system.file("extdata", 
+  "walleye_detections.csv", package = "glatos")
 
-det <- read_glatos_detections(det_file)
+wd <- read_glatos_detections(wd_file)
 
 # Test using testthat library
-test_that("read_glatos_detections gives expected result", {
+test_that("walleye_detections gives expected result", {
   # Check if expected and actual results are the same
-  expect_equal(det, walleye_detections)
+  expect_equal(wd, walleye_detections)
+})
+
+
+#get path to example detection file
+ld_file <- system.file("extdata", 
+  "lamprey_detections.csv", package = "glatos")
+
+ld <- read_glatos_detections(ld_file)
+
+test_that("lamprey_detections gives expected result", {
+  # Check if expected and actual results are the same
+  expect_equal(ld, lamprey_detections)
 })
