5a42ab22ef7d3195a328d1a7371812ce0e5ed7e3
Christopher Holbrook <cholbrook@usgs.gov>
2018-02-15 00:54:15 -0500

    overhaul and simplify min_lag,  documentation, and tests, drawing from T Hayden and A Dini versions

--- R/proc-min_lag.r
+++ R/proc-min_lag.r
@@ -1,22 +1,29 @@
-#' Minimum lag filter
+#' Calculate 'min_lag' for identifying potential false positive detections
 #'
-#' Calculate minimum time interval between successive detections for identifying
-#' false detections
-#' 
-#' Calculates minimum time interval between detections for each tag on
-#' a receiver
+#' Calculate minimum time interval (min_lag) between successive detections and 
+#' add to detection data set for identifying potential false detections.
 #'
-#' @param dtc An object of class \code{glatos_detections} or data
-#'   frame containing acoustic detection data with
-#'   \code{detection_timestamp}, \code{transmitter_id}, and
-#'   \code{receiver_sn}.  \code{min_lag} is loosely based on the the
-#'   "short interval" method described by Pincock (2012) and
-#'   replicates the \code{min_lag} column in the standare glatos
-#'   export file. In this case (GLATOS), \code{min_lat} is defined for each
-#'   detection as the shortest interval (in seconds) between either
-#'   the previous or next detection (whichever is closest) of the same
-#'   transmitter on the same receiver.
+#' @param det A \code{glatos_detections} object (e.g., produced by
+#'   \link{read_glatos_detections}) \emph{or} a data frame containing detection
+#'   data with the following columns: 
+#'   \describe{ 
+#'   \item{detection_timestamp_utc}{Detection timestamps; MUST be of class
+#'   POSIXct.}
+#'   \item{transmitter_codespace}{A character string with transmitter code space
+#'   (e.g., "A69-1061" for Vemco PPM coding").}
+#'   \item{transmitter_id}{A character string with transmitter ID code (e.g.,
+#'   "1363" for Vemco PPM coding").}
+#'   \item{receiver_sn}{A character vector with unique receiver serial number.}
+#'   }
 #' 
+#' @details \code{min_lag} is loosely based on the the "short interval"
+#'   described by Pincock (2012) and replicates the \code{min_lag} column in the
+#'   standard glatos detection export file. In this case (GLATOS),
+#'   \code{min_lag} is defined for each detection as the shortest interval (in
+#'   seconds) between either the previous or next detection (whichever is
+#'   closest) of the same transmitter code (defined here as combination of
+#'   transmitter_codespace and transmitter_id) on the same receiver.
+#'   
 #' @details A new column (\code{min_lag}) is added to the input
 #'     dataframe that represents the time (in seconds) between the
 #'     current detection and the next detection (either before or
@@ -24,46 +31,57 @@
 #'     function replicates the 'min_lag' column included in the
 #'     standard glatos export.
 #'
-#' @return a column is added to input dataframe with the shortest time interval
-#' for each observation
+#' @return A column \code{min_lag} (defined above) is added to input data frame.
 #'
+#' @seealso \code{\link{false_detections}}
 #' 
-#' @author Todd Hayden
-#' @export
+#' @author Chris Holbrook, Todd Hayden, Angela Dini
+#' 
+#' @references
+#'   Pincock, D.G., 2012. False detections: what they are and how to remove them
+#'     from detection data. Vemco Division, Amirix Systems Inc., Halifax,
+#'     Nova Scotia.
+#'     \cr \url{http://www.vemco.com/pdf/false_detections.pdf}
 #' 
 #' @examples
 #'
-#' # load detection file
-#'
+#' # load example detection file
 #' det_file <- system.file("extdata", "walleye_detections.csv",
 #'                          package = "glatos")
-#' dtc <- read_glatos_detections(det_file)
+#' det <- read_glatos_detections(det_file)
+#'
+#' # rename existing min_lag column
+#' colnames(det)[colnames(det) == "min_lag"] <- "min_lag.x"
 #'
 #' # calculate min_lag
-#' min_lag(dtc)
-#'
-#'
+#' det <- min_lag(det)
 #' 
+#' head(det)
+#'
+#' @export 
 
-min_lag <- function(dtc){
-  setDT(dtc)
-  setkey(dtc, transmitter_id, receiver_sn, detection_timestamp_utc)
-  priorTDiff <- as.numeric( dtc[,detection_timestamp_utc]) - as.numeric(
-    c(NA, head(dtc[,detection_timestamp_utc], -1)))
-  futureTDiff <- as.numeric(c(tail(dtc[,detection_timestamp_utc], -1), NA)) -
-    as.numeric(dtc[,dtc$detection_timestamp_utc])
-  futureRec <- as.numeric(c(tail(dtc[,receiver_sn], -1), NA) != dtc[,receiver_sn])
-  priorRec <- as.numeric(c(NA, head(dtc[,receiver_sn], -1)) != dtc[,receiver_sn])
-  futureTran <- as.numeric(c(tail(dtc[,transmitter_id], -1), NA) !=
-                             dtc[,transmitter_id])
-  priorTran <- as.numeric(c(NA, head(dtc[, transmitter_id], -1)) !=
-                            dtc[,transmitter_id])
-
-  futureTDiff[futureRec == 1] <- NA
-  priorTDiff[priorRec ==1] <- NA
-  futureTDiff[futureTran ==1] <- NA
-  priorTDiff[priorTran ==1] <- NA
-  dtc$min_lag <- pmin(priorTDiff, futureTDiff, na.rm = TRUE)
-  setkey(dtc, animal_id, detection_timestamp_utc)
+min_lag <- function(det){
+  
+  #coerce to data.table
+  dtc <- data.table::as.data.table(det)
+  
+  dtc[ , ord := 1:.N] #original order
+  
+  data.table::setkey(dtc, transmitter_codespace, transmitter_id, receiver_sn, 
+    detection_timestamp_utc)
+  
+  #calculate min_lag
+  dtc[ , min_lag := pmin(diff(c(NA, as.numeric(detection_timestamp_utc))), 
+                         diff(c(as.numeric(detection_timestamp_utc),NA)), 
+                         na.rm = TRUE),
+         by = c("transmitter_codespace", "transmitter_id", "receiver_sn")]
+  
+  #return original order
+  setkey(dtc, ord)
+  
+  #drop unwanted columns
+  drop_cols <- "ord"
+  dtc <- dtc[ , !drop_cols, with = FALSE]
+  
   return(as.data.frame(dtc))
 }
---
+++ tests/testthat/test-min_lag.r
@@ -0,0 +1,26 @@
+context("Check min_lag")
+
+# Sample data for min_lag
+time <- c("2010/10/11 08:14:22", "2010/10/11 08:15:22", "2010/10/11 09:00:00", 
+          "2010/10/11 10:23:55", "2010/10/11 11:23:55", "2010/10/11 11:24:55", 
+          "2010/10/11 11:24:56", "2010/10/11 11:52:44", "2010/10/11 11:58:44", 
+          "2010/10/11 11:59:45")
+
+time <- as.POSIXct(time, tz = "UTC")
+trans1 <- c(121, 151, 161, 151, 151, 161, 121, 151, 161, 121)
+trans2 <- c(121, 151, 161, 151, 151, 161, 121, 151, 161, 121)
+rec <- c(4, 2, 3, 2, 2, 2, 4, 2, 2, 4)
+sampleMinLag <- data.frame(detection_timestamp_utc = time, 
+                           transmitter_codespace = trans1, 
+                           transmitter_id = trans2, 
+                           receiver_sn = rec)
+minLagData <- min_lag(sampleMinLag)
+
+# Expected results
+minLagShouldBe <- c(11434, 7713, NA, 3600, 1729, 2029, 2089, 1729, 2029, 2089)
+
+# Testing column that results from getMinLag using testthat library
+test_that("min_lag column gives expected result", {
+  # Check if expected and actual results are the same
+  expect_equal(minLagData$min_lag, minLagShouldBe)
+})
