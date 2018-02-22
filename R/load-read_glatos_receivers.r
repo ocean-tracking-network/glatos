2f0531ce38e85af7a0bd41617b4d8ac070429976
Christopher Holbrook <cholbrook@usgs.gov>
2018-02-21 21:45:53 -0500

    update help and add reference to internal objects

--- R/load-read_glatos_receivers.r
+++ R/load-read_glatos_receivers.r
@@ -1,26 +1,24 @@
-#' @title 
 #' Read data from a GLATOS receiver location file
 #' 
-#' @description
-#' Read data from a standard GLATOS receiver location (csv) file and return a 
+#' Read data from a standard GLATOS receiver location (csv) file and return a
 #' data.frame of class \code{glatos_receivers}.
 #'
 #' @param rec_file A character string with path and name of receiver location
 #'   file in standard GLATOS format (*.csv). If only file name is given, then
-#'   the file must be located in the working directory.
+#'   the file must be located in the working directory. File must be a standard
+#'   GLATOS file provide via GLATOSWeb Data Portal (e.g.,
+#'   \emph{GLATOS_receiverLocations_yyyymmdd_xxxxxx.csv}).
 #'  
-#' @param version An optional character string with the glatos file version
+#' @param version An optional character string with the GLATOS file version
 #'   number. If NULL (default value) then version will be determined by
 #'   evaluating file structure. The only allowed values currently are
 #'   \code{NULL} and \code{"1.0"}. Any other values will trigger an error.
 #'  
-#' @details 
-#' Data are loaded using the \code{fread} function in the
-#' \code{data.table} package and timestamps are coerced to POSIXct using the
-#' \code{fastPOSIXct} function in the \code{fasttime} package. All timestamps
-#' must be in UTC timezone per GLATOS standard.
+#' @details Data are loaded using \link[data.table]{fread} and timestamps are
+#' coerced to POSIXct using \link[fasttime]{fastPOSIXct}. All timestamps must be
+#' 'YYYY-MM-DD HH:MM' format and in UTC timezone per GLATOS standard.
 #' 
-#' @return A data.frame of class \code{glatos_receivers}:
+#' @return A data.frame of class \code{glatos_receivers}.
 #'
 #' @author C. Holbrook (cholbrook@usgs.gov) 
 #'
@@ -43,7 +41,7 @@ read_glatos_receivers <- function(rec_file, version = NULL) {
   #Identify file version
   id_file_version <- function(rec_file){
     col_names <- names(data.table::fread(rec_file, nrows = 0))
-    if(all(glatos_receivers_schema$v1.0$name == col_names)) { 
+    if(all(glatos:::glatos_receivers_schema$v1.0$name == col_names)) { 
       return("1.0") 
     } else {
       stop("Receiver location file version could not be identified.")
@@ -53,7 +51,7 @@ read_glatos_receivers <- function(rec_file, version = NULL) {
   if(is.null(version)) {
     version <- id_file_version(rec_file)
   } else if (!(paste0("v",version) %in% 
-               names(glatos_receivers_schema))) {
+               names(glatos:::glatos_receivers_schema))) {
     stop(paste0("Receiver locations file version ", version,
                 " is not supported."))
   }
@@ -61,7 +59,7 @@ read_glatos_receivers <- function(rec_file, version = NULL) {
   #-v1.0----------------------------------------------------------------  
   if (version == "1.0") {
 
-    col_classes <- glatos_receivers_schema[["v1.0"]]$type
+    col_classes <- glatos:::glatos_receivers_schema[["v1.0"]]$type
     timestamp_cols <- which(col_classes == "POSIXct")
     date_cols <- which(col_classes == "Date")
     col_classes[c(timestamp_cols, date_cols)] <- "character"
@@ -71,18 +69,18 @@ read_glatos_receivers <- function(rec_file, version = NULL) {
     
     #coerce timestamps to POSIXct; note that with fastPOSIXct raw
     #  timestamp must be in UTC; and tz argument sets the tzone attr only
-    for (j in timestamp_cols) set(rec, j = j, 
+    for (j in timestamp_cols) data.table::set(rec, j = j, 
                       value = fasttime::fastPOSIXct(rec[[j]], tz = "UTC"))
     #coerce dates to date
     for (j in date_cols) {
-      set(rec, j = j, value = ifelse(rec[[j]] == "", NA, rec[[j]]))
-      set(rec, j = j, value = as.Date(rec[[j]]))
+      data.table::set(rec, j = j, value = ifelse(rec[[j]] == "", NA, rec[[j]]))
+      data.table::set(rec, j = j, value = as.Date(rec[[j]]))
     }
   }
   #-end v1.0----------------------------------------------------------------
   
   #assign class 
-  rec <- glatos_receivers(rec)
+  rec <- glatos:::glatos_receivers(rec)
   
   return(rec)
 }
