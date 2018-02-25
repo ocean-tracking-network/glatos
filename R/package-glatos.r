42df1d1d4cabfe56611bf523ed370493a0c5b505
Christopher Holbrook <cholbrook@usgs.gov>
2018-02-25 16:41:26 -0500

    add utils namespace call to startup message

--- R/package-glatos.r
+++ R/package-glatos.r
@@ -102,14 +102,14 @@
 #'   text
 #' @importFrom stats approx dnorm ecdf end fivenum median na.omit rbinom rnorm
 #'   runif start step
-#' @importFrom utils capture.output read.csv setTxtProgressBar txtProgressBar
-#'   unzip write.csv write.table zip
+#' @importFrom utils capture.output read.csv packageVersion setTxtProgressBar 
+#'   txtProgressBar unzip write.csv write.table zip
 #' @importFrom grDevices bmp colorRampPalette dev.new dev.off jpeg png rainbow
 #'   tiff
 globalVariables(".") #to avoid R CMD check note
 
 #package startup message
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste0("version ",packageVersion("glatos"), 
+  packageStartupMessage(paste0("version ",utils::packageVersion("glatos"), 
     " ('three-meat-pizza')"))
 }
