0b5f095f9907e85bf8ae5df5524b4c038ac49afe
Alex Nunes <alex.et.nunes@gmail.com>
2018-09-12 11:56:05 -0300

    Changed read_otn_detections to only need essential columns Changes to be committed: 	modified:   data-raw/intdata-otn_detection_schema.r

--- data-raw/intdata-otn_detection_schema.r
+++ data-raw/intdata-otn_detection_schema.r
@@ -1,5 +1,5 @@
 
-#Make glatos_detection_schema (internal data object for read_glatos_detections)
+#Make otn_detection_schema (internal data object for read_otn_detections)
 #Specify column names and data types for each detection file version
 
 
@@ -9,44 +9,18 @@
 # OTN
 
 otn_detection_schema <- read.table(text = "
-         name           type                 mapping
-collectioncode     character          collectioncode
-catalognumber      character               animal_id
-scientificname     character          scientificname
-commonname         character              commonname
-datelastmodified        Date        datelastmodified
-detectedby         character              detectedby
-receiver_group     character             receiver_sn
-station            character            glatos_array
-receiver           character                receiver
-bottom_depth         numeric            bottom_depth
-receiver_depth       numeric          receiver_depth
-tagname            character          transmitter_id
-codespace          character   transmitter_codespace
-sensorname         character              sensorname
-sensorraw            numeric               sensorraw
-sensortype         character              sensortype
-sensorvalue          numeric             sensorvalue
-sensorunit         character              sensorunit
-datecollected        POSIXct detection_timestamp_utc
-timezone           character                timezone
-longitude            numeric             deploy_long
-latitude             numeric              deploy_lat
-st_setsrid_4326    character         st_setsrid_4326
-yearcollected        integer           yearcollected
-monthcollected       integer          monthcollected
-daycollected         integer            daycollected
-julianday            integer               julianday
-timeofday            numeric               timeofday
-datereleasedtagger      Date      datereleasedtagger
-datereleasedpublic      Date      datereleasedpublic
-local_area         character              local_area
-notes              character                   notes
-citation           character                citation
-unqdetecid         character              unqdetecid
-",
-header = TRUE,
-stringsAsFactors = FALSE)
+                                   name           type                 mapping
+                                   catalognumber      character               animal_id
+                                   commonname         character           common_name_e
+                                   receiver_group     character            glatos_array
+                                   station            character                 station
+                                   tagname            character          transmitter_id
+                                   datecollected        POSIXct detection_timestamp_utc
+                                   longitude            numeric             deploy_long
+                                   latitude             numeric              deploy_lat
+                                   ",
+                                   header = TRUE,
+                                   stringsAsFactors = FALSE)
 
 #\OTN
 #----------------------------------------------------
