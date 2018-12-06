82ba56d6fbbfea4866c59e13f0e45415047f2539
Todd Hayden <thayden@usgs.gov>
2018-12-06 12:33:17 -0500

    revised make_transition2 to use fasterize for rasterizing

--- R/vis-make_transition2.r
+++ R/vis-make_transition2.r
@@ -4,32 +4,24 @@
 #' \link[=SpatialPolygons]{SpatialPolygonsDataFrame}.
 #'
 #' @param poly A spatial polygon object of class
-#' \link[=SpatialPolygons]{SpatialPolygonsDataFrame}.
+#'   \link[=SpatialPolygons]{SpatialPolygonsDataFrame} or a
+#'   \link[sf:sf]{sf::sf()} object with a geometry column of polygon and/or
+#'   multipolygon objects.
 #'
 #' @param res two element vector that specifies the x and y dimension
 #'   of output raster cells.  Units of res are same as input
 #'   polygon.
 #'
-#' @param extent_out An optional \code{Extent} object
-#'   (see \link[raster]{extent}) that determines the extent of the
-#'   output objects. Output extent will default to extent of input object
-#'   \code{poly} if \code{extent_out}, and \code{x_lim}/\code{y_lim}
-#'   are NULL (default).
-#'
 #' @param x_lim An optional two-element vector with extents of x axis.
+#'     Default returns object with extent of input object.
 #'
 #' @param y_lim An optional two-element vector with extents of x axis.
-#'
-#' @param water value coded as water in transition layer. Represents
-#'   the "cost" associated with moving between raster cells coded as
-#'   water.
+#'     Default returns object with extent of input objects
 #' 
-#' @param land land value coded as land in transition layer. Represents
-#'   the "cost" associated with impossible (for fish) over land
-#'   movements.
-#' 
-#' @details \code{make_transition} uses \link[raster]{rasterize} to
-#'   convert a \link[=SpatialPolygons]{SpatialPolygonsDataFrame} into
+#' @details \code{make_transition2} uses \link[fasterize]{fasterize} to
+#'   convert a \link[=SpatialPolygons]{SpatialPolygonsDataFrame} or a
+#'   \link[sf:sf]{sf::sf()} object with a geometry column of polygon and/or
+#'   multipolygon into
 #'   a raster layer, and geo-corrected transition layer
 #'   \link[gdistance]{transition}.  Raster cell values on land = 0 and
 #'   water = 1.
@@ -39,16 +31,6 @@
 #'   cells are connected by 16 directions and transition function
 #'   returns 0 (land) for movements between land and water and 1 for
 #'   all over-water movements.
-#'
-#' @details default values for "land" and "water" arguments allow
-#'   interpolation of fish movements over land when receiver is coded
-#'   as on "land" in transition layer.  This often occurs for
-#'   receivers in rivers when pixel size of transition layer is too
-#'   large to distinguish between water and land.  Changing land
-#'   argument to 0 and water to 1 will prevent any interpolation
-#'   overland and result in an error if a receiver is on land.
-#'
-#' 
 #' 
 #' @return A list with two elements:
 #' \describe{
@@ -63,7 +45,6 @@
 #'
 #' @examples
 #'
-#' library(sp) #for loading greatLakesPoly
 #' library(raster) # for plotting rasters
 #'
 #' # get polygon of the Great Lakes
@@ -92,38 +73,48 @@
 #'
 #' @export
 
-make_transition2 <- function(poly, res = c(0.1, 0.1), extent_out = NULL,
-                             x_lim = NULL, y_lim = NULL, water = 1e-10, land = 1000){
+make_transition2 <- function (poly, res = c(0.1, 0.1), x_lim = NULL,
+                              y_lim = NULL){
+    message("Making transition layer...")
+    if (inherits(poly,  c("SpatialPolygonsDataFrame", "SpatialPolygons", "sf")) == FALSE) {
+        stop(paste0("Supplied object for 'poly' argument is not class ",
+                    "SpatialPolygonsDataFrame, SpatialPolygons or a sf polygon object"), call. = FALSE)}        
+    if (sum(is.null(x_lim), is.null(y_lim)) == 1) 
+        stop(paste0("You must specify ", "'x_lim' and 'y_lim' or neither."))
+    if (!is.null(x_lim) & length(x_lim) != 2) 
+        stop("'x_lim' must be a vector ", "with exactly two elements.")
+    if (!is.null(y_lim) & length(y_lim) != 2) 
+        stop("'y_lim' must be a vector ", "with exactly two elements.")
+    if (is.null(x_lim)) {
+        extent_out <- raster::extent(poly)
+    }
+    else if (!is.null(x_lim)) {
+        extent_out <- raster::extent(c(x_lim[1], x_lim[2], y_lim[1], 
+                                       y_lim[2]))
+    }
 
-  # convert from "cost" to "conductance"
-  water <- 1/water
-  land <- 1/land
-
-  message("Making transition layer...")
-  
-  if(sum(is.null(x_lim), is.null(y_lim)) == 1) stop(paste0("You must specify ",
-    "'x_lim' and 'y_lim' or neither."))
-  if(!is.null(x_lim) & length(x_lim) != 2) stop("'x_lim' must be a vector ",
-    "with exactly two elements.")
-  if(!is.null(y_lim) & length(y_lim) != 2) stop("'y_lim' must be a vector ",
-    "with exactly two elements.")
-
-  if(is.null(x_lim) & is.null(extent_out)){ 
-    extent_out <- raster::extent(poly) 
-    } else if (!is.null(x_lim)) { 
-      extent_out <- raster::extent(c(x_lim[1], x_lim[2],
-                                      y_lim[1], y_lim[2]))
-  }
+    if (inherits(poly, c("SpatialPolygonsDataFrame", "SpatialPolygons")) == TRUE){
+        poly <- sf::st_as_sf(poly)
+    }
     
-  burned = raster::rasterize(poly, y = raster::raster(res = res, ext = extent_out), 
-    field = water, background = land)
-  
-  tran <- function(x) if(x[1] == land | x[2] == land){ return(land) } else { return(water) }
-  tr1 <- gdistance::transition(burned, transitionFunction = tran, 
-    directions = 16)
-  tr1 <- gdistance::geoCorrection(tr1, type="c")
-  
-  message("Done.")
-  return(list(transition = tr1, rast = burned))
-  }
-    
+    if (sf::st_crs(poly) == sf::NA_crs_){
+        warning(paste0("poly object does not have a coordinate reference system defined!"))
+    }
+    burned = fasterize::fasterize(sf = poly,
+                                  raster = raster::raster(res = res,
+                                                          ext = extent_out),
+                                  field = NULL, fun = "last", background = 0,
+                                  by = NULL)
+
+    tran <- function(x) if (x[1] * x[2] == 0) {
+                            return(0)
+                        }
+                        else {
+                            return(1)
+                        }
+    tr1 <- gdistance::transition(burned, transitionFunction = tran,
+                                 directions = 16)
+    tr1 <- gdistance::geoCorrection(tr1, type = "c")
+    message("Done.")
+    return(list(transition = tr1, rast = burned))
+}
