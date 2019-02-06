ea8f773b71d1ec1d25fe68f5f7ed609cfb416ae7
Todd Hayden <thayden@usgs.gov>
2019-02-06 11:21:40 -0500

    forgot to commit last time

--- vignettes/render.R
+++ vignettes/render.R
@@ -1,6 +1,6 @@
 library(rmarkdown)
 library(tint)
 
-render(input = "~/Documents/glatos/vignettes/detection_range_vignette.Rmd", output_file = "detection_range_vignette.pdf", output_format = tint::tintPdf(highlight = "haddock"), clean = FALSE)
+render(input = "~/Documents/glatos/vignettes/detection_range_vignette.Rmd", output_file = "detection_range_vignette.pdf", output_format = tint::tintPdf(highlight = "haddock"))
 
 
