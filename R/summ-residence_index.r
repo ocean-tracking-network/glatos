a554e552843273fb80f1f1850485ad8961fa6076
Alex Nunes <alex.et.nunes@gmail.com>
2019-02-20 17:48:31 -0400

    /Users/alexnunes/Downloads/nunes_alex.pdf
    Cleaning changes to RI:
    - made vignette formulas consistent
    - changed default groupwise_total to TRUE
    -removed all plotly and plotting from RI and REI
    
     Changes to be committed:
    	modified:   R/summ-residence_index.r
    	modified:   inst/userguides/receiver_efficiency.Rmd
    	modified:   inst/userguides/residence_index.Rmd
    	modified:   vignettes/residence_index.pdf

--- R/summ-residence_index.r
+++ R/summ-residence_index.r
@@ -161,7 +161,7 @@
 #' @export
 residence_index <- function(detections, calculation_method='kessel', 
   locations = NULL, group_col = "animal_id", time_interval_size = "1 day", 
-  groupwise_total = FALSE) {
+  groupwise_total = TRUE) {
   
   #set to NULL if NA
   if(!is.null(group_col)) if(is.na(group_col)) group_col <- NULL 
@@ -406,64 +406,5 @@ get_days <- function(dets, calculation_method='kessel',
       stop("Unsupported 'calculated_method'.")
   }
   return(days)
-}
-
-
-
-#' Uses plotly to place the caluclated residence index on a map.
-#'
-#' @details This function uses plotly to place the caluclated RI on a map.
-#'
-#' @param df A data.frame from the \code{residence_index} containing
-#' days_detected, residency_index, location, mean_latitude,
-#' mean_longitude
-#'
-#' @param title A string for the title of the plot
-#'
-#' @return A plotly plot_geo object
-#'
-#' @importFrom magrittr "%>%"
-#' @import plotly
-#' @export
-ri_plot <- function(df, title="Residence Index") {
-  lat_range <- c(df$mean_latitude[which.min(df$mean_latitude)], df$mean_latitude[which.max(df$mean_latitude)])
-  lon_range <- c(df$mean_longitude[which.min(df$mean_longitude)], df$mean_longitude[which.max(df$mean_longitude)])
-  m <- list(colorbar = list(title = "Residence Index"))
-
-  g <- list(
-    showland = TRUE,
-    landcolor = plotly::toRGB("gray"),
-    subunitcolor = plotly::toRGB("white"),
-    countrycolor = plotly::toRGB("white"),
-    showlakes = TRUE,
-    lakecolor = plotly::toRGB("white"),
-    showsubunits = TRUE,
-    showcountries = TRUE,
-    resolution = 50,
-    projection = list(
-      type = 'conic conformal',
-      rotation = list(lon = -100)
-    ),
-    lonaxis = list(
-      showgrid = TRUE,
-      gridwidth = 0.5,
-      range = lon_range,
-      dtick = 1
-    ),
-    lataxis = list(
-      showgrid = TRUE,
-      gridwidth = 0.5,
-      range = lat_range,
-      dtick = 1
-    )
-  )
-
-  p <- plot_geo(df, lat = ~mean_latitude, lon = ~mean_longitude, color = ~residency_index) %>%
-    add_markers(size = ~((df$residency_index * 5) + 1),
-                text = ~paste(df$location, ":",df$residency_index), hoverinfo = "text"
-    ) %>%
-    layout(title = title, geo = g)
-  show(p)
-  return(p)
 }
 
