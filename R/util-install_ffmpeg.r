65d6dbccfa6df1605b720b687f11e5a3b8d5c8c1
Todd Hayden <thayden@usgs.gov>
2019-02-20 14:21:48 -0500

    added documentation and example to function

--- R/util-install_ffmpeg.r
+++ R/util-install_ffmpeg.r
@@ -1,7 +1,30 @@
-##' install ffmpeg
-##' @details checks for and installs ffmpeg into package directory for making animations
-##' @return value
-##' @author Todd Hayden
+##' Install external FFmpeg software into `glatos` package
+##' 
+##' Downloads and installs external ffmpeg into `glatos` package
+##' directory system to enable creation animated videos of fish
+##' movements
+##'
+##' @details \code{install_ffmpeg} determines operating system
+##'   (Windows or Mac) and downloads the latest stable version of
+##'   FFmpeg software.  FFmpeg software is installed within glatos
+##'   package and may be accessed using \code{system.file}
+##'
+##' 
+##' @return message is returned to console signalling successful installation
+##' @author Todd Hayden, Chris Holbrook
+##'
+##' @examples
+##'
+##'\dontrun{
+##'
+##' # call function
+##' install_ffmpeg()
+##'
+##' # determine version of FFmpeg installed and if install was successful
+##' fle <- system.file("ffmpeg/bin/ffmpeg.exe", package = "glatos")
+##' system2(fle, "-version")
+##' 
+##'}
 ##' @export
 
 # install ffmpeg to GLATOS package
@@ -23,31 +46,27 @@ get_os <- function(){
 
 
 install_ffmpeg <- function(){
-    # determine what OS
-    os <- get_os()
-
-    # download windows build of ffmpeg
-    if(os == "windows"){
-
-        url <- "http://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip"
-    }
-
-    # download osx build of ffmpeg
-    if(os == "osx"){
-        url <- "http://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-latest-macos64-static.zip"
-    }
-   
-    # find package files on system
-    tmp <- tempdir()
-    destfile <- file.path(tmp, "ffmpeg.zip")
-    download.file(url, destfile = destfile, mode = "wb")
-    pkg <- find.package("glatos")
-    utils::unzip(destfile, exdir = pkg)
-    fls <- list.files(pkg)
-    file.rename(file.path(pkg, fls[grep("ffmpeg-*", fls) ]), file.path(pkg, "ffmpeg"))
+  # determine what OS
+  os <- get_os()
+  
+  # download windows build of ffmpeg
+  if(os == "windows"){
+    
+    url <- "http://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip"
+  }
+  
+  # download osx build of ffmpeg
+  if(os == "osx"){
+    url <- "http://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-latest-macos64-static.zip"
+  }
+  
+  # find package files on system
+  tmp <- tempdir()
+  destfile <- file.path(tmp, "ffmpeg.zip")
+  download.file(url, destfile = destfile, mode = "wb")
+  pkg <- find.package("glatos")
+  utils::unzip(destfile, exdir = pkg)
+  fls <- list.files(pkg)
+  file.rename(file.path(pkg, fls[grep("ffmpeg-*", fls) ]), file.path(pkg, "ffmpeg"))
+  message("done")
 }
-
-
-# shoule be able to use "system.file" to call ffmpeg
-#system.file("ffmpeg/bin/ffmpeg.exe", package = "glatos")
-#install_ffmpeg()
