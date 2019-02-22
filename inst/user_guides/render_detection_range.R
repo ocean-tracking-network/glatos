595aa342eff7e57dd4efe68c6d6324ec3a468bbd
Christopher Holbrook <cholbrook@usgs.gov>
2019-02-22 07:27:04 -0500

    update vignette formats

---
+++ inst/user_guides/render_detection_range.R
@@ -0,0 +1,8 @@
+library(rmarkdown)
+library(tint)
+
+render(input = "inst/user_guides/detection_range_vignette.Rmd", 
+  output_file = "detection_range_vignette.pdf", 
+  output_format = tint::tintPdf(highlight = "haddock"))
+
+
