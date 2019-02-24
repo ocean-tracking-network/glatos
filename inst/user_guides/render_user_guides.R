8c148d09db7f7a6a56332895085e0cd0b4269a06
Todd Hayden <thayden@usgs.gov>
2019-02-23 23:03:51 -0500

    converted from tufte format to standard vignette format.

--- inst/user_guides/render_user_guides.R
+++ inst/user_guides/render_user_guides.R
@@ -1,6 +1,4 @@
 library(rmarkdown)
-library(tint)
-
 
 render(input = "inst/user_guides/data_requirements.Rmd", 
   output_dir = "vignettes",
@@ -19,13 +17,10 @@ render(input = "inst/user_guides/residence_index.Rmd",
   output_dir = "vignettes",
   output_format = "all")
 
-
-
 render(input = "inst/user_guides/detection_range_vignette.Rmd", 
-  output_file = "detection_range_vignette.pdf", 
-  output_format = tint::tintPdf(highlight = "haddock"))
-
+  output_dir = "vignettes"
+  output_format = "all")
 
 render(input = "inst/user_guides/gganimate_vignette.Rmd", 
-  output_file = "gganimate.pdf",
-  output_format = tint::tintPdf(highlight = "haddock"), clean=TRUE)
+  output_dir = "vignettes",
+  output_format = "all")
