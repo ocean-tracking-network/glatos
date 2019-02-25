723e4a4aa743cd774b845d75aa9d97cf64212e8e
Christopher Holbrook <cholbrook@usgs.gov>
2019-02-24 23:07:16 -0500

    apply NN edits

--- inst/supplemental_docs/render_supplemental_docs.R
+++ inst/supplemental_docs/render_supplemental_docs.R
@@ -21,10 +21,11 @@ render(input = "inst/supplemental_docs/gganimate_handout.Rmd",
 
 #render html versions of vignettes
 render(input = "vignettes/data_loading_vignette.Rmd", 
-  output_dir = "vignettes",
-  output_format = "html_vignette")
+  output_dir = "vignettes")
+#output format -> make sure html_document is first in Rmd 
 
 render(input = "vignettes/data_requirements_vignette.Rmd", 
-  output_dir = "vignettes",
-  output_format = "html_vignette")
+  output_dir = "vignettes")
+#output format -> make sure html_document is first in Rmd 
+
 
