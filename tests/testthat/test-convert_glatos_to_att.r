f48a47bb759ec45f6c7e0384b77d7cb2df5b16b0
Christopher Holbrook <cholbrook@usgs.gov>
2019-10-11 14:18:39 -0400

    add test for convert_glatos_to_att output and add example walleye att internal data to check against

---
+++ tests/testthat/test-convert_glatos_to_att.r
@@ -0,0 +1,29 @@
+context("Check convert_glatos_to_att")
+
+#check against internal data object 'walleye_att' in R/sysdata.r
+
+# Actual result
+#get path to example detection file
+wd_file <- system.file("extdata", 
+  "walleye_detections.csv", package = "glatos")
+
+wald <- read_glatos_detections(wd_file)
+
+#get path to example receiver file
+rec_file <- system.file("extdata", "sample_receivers.csv", 
+  package = "glatos")
+recd <- read_glatos_receivers(rec_file) # load receiver data
+
+watt <- convert_glatos_to_att(wald, recd)
+
+
+
+# Test using testthat library
+test_that("walleye_att gives expected result", {
+  # Check if expected and actual results are the same
+  expect_equal(watt, walleye_att)
+})
+
+
+
+
