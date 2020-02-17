9428c3091f88206d928ef832b404bc38f3c07482
chrisholbrook <cholbrook@usgs.gov>
2020-02-17 12:51:27 -0500

    add internal data object hfx_deployments and dput expected objects for REI and RI tests instead of in sysdata.r (origin unknown; not in data-raw)

---
+++ data-raw/intdata-hfx_deployments.r
@@ -0,0 +1,17 @@
+#Make hfx_deployments in rda
+# (internal data object)
+
+#get path to example receiver_locations file
+rec_file <- system.file("extdata",
+  "hfx_deployments.csv", package = "glatos")
+
+hfx_deployments <- read_otn_deployments(rec_file)
+
+#----------------------------------------------------
+
+#add to sysdata.rda
+rda_file <- file.path("R/sysdata.rda")
+glatos:::add_internal_data(hfx_deployments, rda_file)
+
+#for exported ('public') data
+#devtools::use_data(glatos_receivers, pkg = "..", overwrite = TRUE)
