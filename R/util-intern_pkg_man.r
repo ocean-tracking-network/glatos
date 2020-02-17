982d7ffdc7f47ea42c217e6ee72ce2cc93e9cde7
chrisholbrook <cholbrook@usgs.gov>
2020-02-17 13:01:18 -0500

    add version = 2 to prevent changing dependency to R 5.3 due to message 'because serialized objects in serialize/load version 3'

--- R/util-intern_pkg_man.r
+++ R/util-intern_pkg_man.r
@@ -6,7 +6,7 @@ add_internal_data <- function(x, rda_file) {
   e <- new.env()
   load(rda_file, e) #load existing internals to new envir
   assign(deparse(substitute(x)), x, envir = e) #add/replace with new object
-  save(list = ls(e), file = rda_file, envir = e)
+  save(list = ls(e), file = rda_file, envir = e, version = 2)
 }
 
 #--------------------------------------------------------------------------
@@ -15,5 +15,5 @@ remove_internal_data <- function(x, rda_file) {
   e <- new.env()
   load(rda_file, e) #load existing internals to new envir
   rm(list = deparse(substitute(x)), envir = e) #remove unwanted object
-  save(list = ls(e), file = rda_file, envir = e)
+  save(list = ls(e), file = rda_file, envir = e, version = 2)
 }
