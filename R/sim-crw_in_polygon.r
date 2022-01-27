81fe0e508b5f1644e3c9d61512477c4ba4d8e80c
chrisholbrook <cholbrook@usgs.gov>
2022-01-26 23:43:01 -0500

    revert to sp_out

--- R/sim-crw_in_polygon.r
+++ R/sim-crw_in_polygon.r
@@ -1,110 +1,109 @@
 #' Simulate a correlated random walk inside a polygon
-#' 
-#' Uses \link{crw} to simulate a random walk as series of equal-length steps 
-#'   with turning angles drawn from a normal distribution inside a polygon.
 #'
-#' @param polyg A \code{\link[sp]{SpatialPolygons}} object.
-#' \cr \emph{OR} \cr
-#' A polygon defined as data frame with numeric columns x and y.
-#' 
-#' @param theta A 2-element numeric vector with turn angle parameters 
-#'   (theta[1] = mean; theta[2] = sd) from normal distribution.
-#'   
-#' @param stepLen A numeric scalar with total distance moved in each step. 
-#'  Units are same as the units of the coordinate reference system specified 
-#'  by argument \code{EPSG} (meters for the default Great Lakes projected 
-#'  coordinate system).
-#'   
+#' Uses \link{crw} to simulate a random walk as series of equal-length steps
+#' with turning angles drawn from a normal distribution inside a polygon.
+#'
+#' @param polyg A spatial polygon object (class must be
+#'   \code{\link[sp]{SpatialPolygonsDataFrame}},
+#'   \code{\link[sp]{SpatialPolygons}}, or \code{\link[sf]{sf}} or
+#'   \code{\link[sf]{sfc}} object containing a 'POLYGON' feature); \cr \emph{OR}
+#'   \cr A polygon defined as data frame with numeric columns x and y.
+#'
+#' @param theta A 2-element numeric vector with turn angle parameters (theta[1]
+#'   = mean; theta[2] = sd) from normal distribution.
+#'
+#' @param stepLen A numeric scalar with total distance moved in each step. Units
+#'   are same as the units of the coordinate reference system specified by
+#'   argument \code{EPSG} (meters for the default Great Lakes projected
+#'   coordinate system).
+#'
 #'
 #' @param initPos A 2-element numeric vector with initial position
-#'   (initPos[1]=x, initPos[2]=y) in same units as \code{polyg}.
-#'   
-#' @param initHeading A numeric scalar with initial heading in degrees.
-#' 
-#' @param nsteps A numeric scalar with number of steps to simulate.
-#' 
-#' @param EPSG Numeric EPSG code of the coordinate system used for simulations. 
-#'   Default is 3175, a projected coordinate system for the North American 
-#'   Great Lakes Basin and St. Lawrence River system. 
-#'   \url{http://spatialreference.org/ref/epsg/nad83-great-lakes-and-st-lawrence-albers/} 
-#' 
-#' @param output_class Specifies class of output object. Possible values are
-#'   \code{"sf"} (default), \code{"sp"} (for \code{SpatialPointsDataFrame}),
-#'   \code{"df"}, (for \code{data.frame}, or \code{"matrix"}.  If TRUE (default)
-#'   then output is a spatial object of \link[sf]{sf}. If FALSE, then output is
-#'   a data.frame.
+#'   (initPos[1]=x, initPos[2]=y) in same coordinate reference system as
+#'   \code{polyg}.
 #'
-#' @param sp_out DEPRECATED but maintained for backward compatibility. If
-#'   specified and \code{sp_out = TRUE}, then \code{output_class} is set to
-#'   \code{"sp"}. If specificed and ]code{sp_out = FALSE}, then
-#'   ]code{output_class} is set to \code{"df"}.
-#'   
-#' @param show_progress Logical. Progress bar and status messages will be 
-#'  shown if TRUE (default) and not shown if FALSE.
-#'   
-#' @details If initPos = NA, then a starting point is randomly
-#'   selected within the polygon boundary. A path is simulated forward
-#'   using the crw function.  Initial heading is also randomly
-#'   selected if initHeading = NA. When a step crosses the polygon
-#'   boundary, a new heading for that step is drawn and the turn angle
-#'   standard deviation is enlarged slightly for each subsequent point
-#'   that lands outside the polygon.
+#' @param initHeading A numeric scalar with initial heading in degrees. E.g., 0
+#'   = North; 90 = East, 180 = South, 270 = West; etc.
+#'
+#' @param nsteps A numeric scalar with number of steps to simulate.
+#'
+#' @param EPSG Numeric EPSG code of coordinate system used for simulations.
+#'   Default is 3175, a projected coordinate system for the North American Great
+#'   Lakes Basin and St. Lawrence River system.
+#'   \url{https://spatialreference.org/ref/epsg/nad83-great-lakes-and-st-lawrence-albers/}.
+#'    Must be a projected (Cartesian) coordinate system.
+#'
+#' @param sp_out A logical scalar that determines if returned object is a
+#'   so-called spatial class. If \code{TRUE} (default value) and input
+#'   \code{polyg} is class \code{\link[sp]{SpatialPolygonsDataFrame}},
+#'   \code{\link[sp]{SpatialPolygons}}, \code{\link[sf]{sf}}, or
+#'   \code{\link[sf]{sfc}} (must be 'POLYGON' feature) then class of returned
+#'   object will be same as input object \code{polyg}. If \code{TRUE} and input
+#'   \code{polyg} is a \code{data.frame} then returned object will be of class
+#'   \code{\link[sp]{SpatialPolygonsDataFrame}}. If \code{FALSE} then returned
+#'   object will be of class \code{data.frame}.
+#'
+#' @param show_progress Logical. Progress bar and status messages will be shown
+#'   if TRUE (default) and not shown if FALSE.
+#'
+#' @details If initPos = NA, then a starting point is randomly selected within
+#'   the polygon boundary. A path is simulated forward using the crw function.
+#'   Initial heading is also randomly selected if initHeading = NA. When a step
+#'   crosses the polygon boundary, a new heading for that step is drawn and the
+#'   turn angle standard deviation is enlarged slightly for each subsequent
+#'   point that lands outside the polygon.
 #'
 #' @details If polyg object is a data frame with x and y columns and
-#'   \code{output_class = "sp"}, then \link[sp]{SpatialPoints} output object
+#'   \code{sp_out = TRUE}, then \code{\link[sp]{SpatialPoints}} output object
 #'   will have coordinate system of \code{EPSG}.  Coordinate system on output
-#'   will be same as input if polyg object is \code{[sp]{SpatialPolygons}} 
-#'   or \code{[sf]{sf}}.
+#'   will be same as input if polyg object is \code{\link[sp]{SpatialPolygons}}
+#'   or \code{\link[sf]{sf}}.
 #'
-#' 
-#' @return A \link[sf]{sf}, \link[sp]{SpatialPoints}, \code{data.frame}, or 
-#' \code{matrix} object in the same CRS as the input 
-#' \code{polyg} object. 
-#' \cr \emph{OR} \cr 
-#' A two-column data frame containing:
-#' \item{x}{x coordinates}
-#' \item{y}{y coordinates}
-#' in the same units as \code{polyg}.
-#' \cr See argument \code{output_class}.
+#'
+#' @return A \code{\link[sf]{sf}}, \code{\link[sp]{SpatialPoints}}, or
+#'   \code{data.frame}, object in the same coordinate system as the input
+#'   \code{polyg} object. \cr \emph{OR} \cr A two-column data frame containing:
+#'   \item{x}{x coordinates} \item{y}{y coordinates} in the same units as
+#'   \code{polyg}. \cr See argument \code{sp_out}.
 #'
 #' @author C. Holbrook \email{cholbrook@@usgs.gov}
-#' 
+#'
 #' @seealso \link{crw}
 #'
-#' @note 
-#' The path is constructed in segments based on the minimum distance between 
-#' the previous point and the closest polygon boundary.
-#' 
-#' Simulations are conducted within the coordinate system specified by 
-#' argument \code{EPSG}. The default EPSG (3175), covers only the Great Lakes
-#' of North America. Simulations conducted in other areas will need to specify 
-#' a valid EPSG for the study area.
+#' @note The path is constructed in segments based on the minimum distance
+#'   between the previous point and the closest polygon boundary.
+#'
+#'   Simulations are conducted within the coordinate system specified by
+#'   argument \code{EPSG}. The default EPSG (3175), covers only the Great Lakes
+#'   of North America. Simulations conducted in other areas will need to specify
+#'   a valid EPSG representing an appropriate projected (Cartesian) coordinate
+#'   system for the study area.
 #'
 #' @examples
-#' 
+#'
 #' #Simple box example
 #' mypolygon <- data.frame(x = c(-50,-50, 50, 50), y = c(-50,50,50,-50))
-#' foo <- crw_in_polygon(mypolygon, theta = c(0, 20), stepLen = 10, 
+#' foo <- crw_in_polygon(mypolygon, theta = c(0, 20), stepLen = 10,
 #'   initPos=c(0,0), initHeading=0, nsteps=50)
 #' class(foo) #note object is SpatialPoints
-#' plot(sp::coordinates(foo), type = "o", pch = 20, asp = c(1,1), 
+#' plot(sp::coordinates(foo), type = "o", pch = 20, asp = c(1,1),
 #'   xlim = range(mypolygon$x), ylim = range(mypolygon$y))
 #' polygon(mypolygon, border = "red")
-#' 
-#' 
+#'
+#'
 #' #Great Lakes Example
 #' data(greatLakesPoly)
-#' 
+#'
 #' #simulate in great lakes polygon
 #' foo2 <- crw_in_polygon(greatLakesPoly,theta=c(0,25), stepLen=10000,
 #'   initHeading=0, nsteps=100, sp_out = TRUE)
-#' 
+#'
 #' #plot
 #' sp::plot(greatLakesPoly, col = "lightgrey", border = "grey")
 #' points(foo2,type="o", pch = 20, col = "red")
-#' 
+#'
 #' #zoom in
-#' sp::plot(greatLakesPoly, col = "lightgrey", border = "grey", 
+#' sp::plot(greatLakesPoly, col = "lightgrey", border = "grey",
 #'   xlim = sp::bbox(foo2)[1,], ylim = sp::bbox(foo2)[2,])
 #' points(foo2,type="o", pch = 20, col = "red")
 #'
@@ -112,11 +111,21 @@
 
 crw_in_polygon <- function(polyg, theta = c(0,10), stepLen = 100, 
   initPos = c(NA,NA), initHeading = NA, nsteps = 30, 
-  EPSG = 3175, output_class = "sf", sp_out = TRUE, show_progress = TRUE){          
+  EPSG = 3175, sp_out = TRUE, show_progress = TRUE){          
   
-  #coerce to sf_polygon if polyg is data.frame
-  if(inherits(polyg, "data.frame")){
-    
+  #check polyg
+  if(!inherits(polyg, c("data.frame", "sf", "sfc", "SpatialPolygonsDataFrame", 
+                        "SpatialPolygons"))) 
+    stop("Input 'polyg' must be of class 'data.frame', 'sf', 'sfc', ",
+          "'SpatialPolygonsDataFrame', or 'SpatialPolygons'.")
+  
+  #check that sf geometry is POLYGON
+  if(inherits(polyg, c("sf", "sfc"))) {
+    if(!("POLYGON" %in% sf::st_geometry_type(polyg)))
+    stop("Input object 'polyg' must contain geometry of type 'POLYGON' when ",
+          "class is 'sf'.")
+    polyg_sf <- polyg
+  } else if(inherits(polyg, "data.frame")){
     #check for names
     if(!all(c("x", "y") %in% names(polyg))) stop("Input data.frame 'polyg' ",
                                                  "must have columns named ",
@@ -135,27 +144,15 @@ crw_in_polygon <- function(polyg, theta = c(0,10), stepLen = 100,
   if(inherits(polyg, c("SpatialPolygonsDataFrame", "SpatialPolygons"))){
 
     polyg_sf <- sf::st_as_sf(polyg)
-    
-    #set CRS
-    polyg_sf <- sf::st_transform(polyg_sf, crs = EPSG)
   }
+  
+  #set CRS
+  polyg_sf <- sf::st_transform(polyg_sf, crs = EPSG)
   
   #if any initPos were not given
   #randomly select one point in the study area
-  if(any(is.na(initPos))){
-    inPoly <- FALSE #logical flag; preallocate
-    while(inPoly == FALSE){
-      init <- c(runif(1, floor(sf::st_bbox(polyg_sf)["xmin"]),
-                      ceiling(sf::st_bbox(polyg_sf)["xmax"])),
-                runif(1, floor(sf::st_bbox(polyg_sf)["ymin"]),
-                      ceiling(sf::st_bbox(polyg_sf)["ymax"])))
-      init <- sf::st_as_sf(data.frame(x=init[1], y = init[2]),
-                           coords = c("x", "y"),
-                           crs = EPSG)
-      inPoly <- any(sf::st_contains(polyg_sf, init, sparse = FALSE))
-    } #end while
-  } #end if
-  
+  if(any(is.na(initPos))) init <- sf::st_sample(polyg_sf, size = 1)
+
   
   #if initPos are both given, check to see if in polyg
   if(all(!is.na(initPos))) {
@@ -173,7 +170,8 @@ crw_in_polygon <- function(polyg, theta = c(0,10), stepLen = 100,
   #randomly select heading if not given
   if(is.na(initHeading)) initHeading <- runif(1, 0, 360)
   
-  path_fwd <- data.frame(x = rep(NA, nsteps + 1), y = NA) #preallocate
+  #preallocate
+  path_fwd <- data.frame(x = rep(NA, nsteps + 1), y = NA) 
   path_fwd[1,] <- sf::st_coordinates(init)
   
   #create lines object from polyg (for distance measurement)
@@ -248,19 +246,7 @@ crw_in_polygon <- function(polyg, theta = c(0,10), stepLen = 100,
   
   if(show_progress) message("Done.")
   
-  
-  #convert to input coordinate system 
-  #path_fwd_sp <- sp::SpatialPoints(path_fwd, proj4string = sp::CRS(projargs))
-  #path_fwd_sp <- sp::spTransform(path_fwd_sp, CRSobj = projargs_in)
-  
-  #for backward compatibility, set output_class based on sp_out if given
-  if(!missing(sp_out) & missing(output_class)){
-     if(sp_out) output_class <- "sp"
-     if(!sp_out) output_class <- "df"
-  }
-  
-  
-  #set output CRS to polygon CRS if output is spatial object; EPSG otherwise
+  #set output CRS to polygon CRS if polygon has CRS; EPSG otherwise
   if(!is.na(sf::st_crs(polyg))) { 
     crs_out <- sf::st_crs(polyg) } else { crs_out <- sf::st_crs(EPSG) }
 
@@ -269,20 +255,15 @@ crw_in_polygon <- function(polyg, theta = c(0,10), stepLen = 100,
                               crs = EPSG)
   path_fwd_sf <- sf::st_transform(path_fwd_sf, crs = crs_out)
   
-  if(output_class == "sf") path_out <- path_fwd_sf
-  
-  if(output_class == "df") 
-    path_out <- as.data.frame(sf::st_coordinates(path_fwd_sf))
-  
-  if(output_class == "matrix") 
-    path_out <- sf::st_coordinates(path_fwd_sf)
-  
-  if(output_class == "sp")
-    path_out <- sf::as_Spatial(path_fwd_sf)
+  if(sp_out){
+    if(inherits(polyg, c("sf", "sfc"))) return(path_fwd_sf)
 
-  
-  return(path_out)
-} #end
+    #return sp object if input is sp or data.frame
+    return(sf::as_Spatial(path_fwd_sf))  
+  } else {
+    return(as.data.frame(sf::st_coordinates(path_fwd_sf)))
+  }
+} 
  
 #check if in polygon
 check_in_polygon <- function(points, polygon, EPSG){
--- tests/testthat/test-crw_in_polygon.r
+++ tests/testthat/test-crw_in_polygon.r
@@ -3,9 +3,9 @@ context("Check crw_in_polygon")
 # non-spatial input
 mypolygon <- data.frame(x = c(-50,-50, 50, 50), y = c(-50,50,50,-50))
 set.seed(30)
-path_dfin_sfout <- crw_in_polygon(mypolygon, theta = c(0, 20), stepLen = 10, 
+path_dfin_spout <- crw_in_polygon(mypolygon, theta = c(0, 20), stepLen = 10, 
                          initPos=c(0,0), initHeading=0, nsteps=5,
-                         output_class = "sf", show_progress = FALSE)
+                         sp_out = TRUE, show_progress = FALSE)
 set.seed(30)
 path_dfin_dfout <- crw_in_polygon(mypolygon, theta = c(0, 20), stepLen = 10, 
                          initPos=c(0,0), initHeading=0, nsteps=5, 
@@ -15,35 +15,25 @@ path_dfin_dfout <- crw_in_polygon(mypolygon, theta = c(0, 20), stepLen = 10,
 # spatial input
 data(greatLakesPoly)
 set.seed(30)
-path_spin_sfout <- crw_in_polygon(greatLakesPoly, theta=c(0,25), stepLen=10000,
-    initPos = c(-87.49017, 48.42314), initHeading=0, nsteps=5,
-    output_class = "sf", show_progress = FALSE)
+path_spin_spout <- crw_in_polygon(greatLakesPoly, theta=c(0,25), stepLen=10000,
+    initPos = c(-87.49017, 48.42314), initHeading=0, nsteps=5, sp_out = TRUE, 
+    show_progress = FALSE)
 set.seed(30)
 path_spin_dfout <- crw_in_polygon(greatLakesPoly, theta=c(0,25), stepLen=10000,
     initPos = c(-87.49017, 48.42314), initHeading=0, nsteps=5, sp_out = FALSE,
     show_progress = FALSE)
 
+path_spin_spout_shouldBe <- 
+new("SpatialPointsDataFrame", data = structure(list(), .Names = character(0), class = "data.frame", row.names = c(NA, 
+6L)), coords.nrs = numeric(0), coords = structure(c(-87.49017, 
+-87.5682764367357, -87.6624051520051, -87.7763030863065, -87.8344478577291, 
+-87.9595460531474, 48.42314, 48.4965252233601, 48.5611619523508, 
+48.609942977839, 48.6911737155544, 48.7263225805848), .Dim = c(6L, 
+2L), .Dimnames = list(NULL, c("coords.x1", "coords.x2"))), bbox = structure(c(-87.9595460531474, 
+48.42314, -87.49017, 48.7263225805848), .Dim = c(2L, 2L), .Dimnames = list(
+c("coords.x1", "coords.x2"), c("min", "max"))), proj4string = new("CRS", 
+projargs = "+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs"))
 
-path_dfin_sfout_coords_shouldBe <- 
-structure(c(0, -4.34765355285354, -9.75360353139628, -16.5935673247779, 
--19.6317805370012, -16.4086037689043, 0, 9.00543772308487, 17.4182674984633, 
-24.7131215275189, 34.24041182839, 43.7067274538907), .Dim = c(6L, 
-2L), .Dimnames = list(c("1", "2", "3", "4", "5", "6"), c("X", 
-"Y")))
-
-path_dfin_dfout_shouldBe <- 
-structure(list(X = c(0, -4.34765355285354, -9.75360353139628, 
--16.5935673247779, -19.6317805370012, -16.4086037689043), Y = c(0, 
-9.00543772308487, 17.4182674984633, 24.7131215275189, 34.24041182839, 
-43.7067274538907)), class = "data.frame", row.names = c("1", 
-"2", "3", "4", "5", "6"))
-
-path_spin_sfout_coords_shouldBe <- 
-structure(c(-87.49017, -87.5786307516362, -87.6622782014656, 
--87.7762979544992, -87.9067801606844, -88.0320278509935, 48.42314, 
-48.4911384479114, 48.5618349867473, 48.610492179597, 48.6351766847532, 
-48.6698817124321), .Dim = c(6L, 2L), .Dimnames = list(c("1", 
-"2", "3", "4", "5", "6"), c("X", "Y")))
 
 path_spin_dfout_shouldBe <- 
 structure(list(X = c(-87.49017, -87.5682764367357, -87.6624051520051, 
@@ -52,10 +42,28 @@ structure(list(X = c(-87.49017, -87.5682764367357, -87.6624051520051,
 48.7263225805848)), class = "data.frame", row.names = c("1", 
 "2", "3", "4", "5", "6"))
 
+path_dfin_spout_coords_shouldBe <- 
+new("SpatialPointsDataFrame", data = structure(list(), .Names = character(0), class = "data.frame", row.names = c(NA, 
+6L)), coords.nrs = numeric(0), coords = structure(c(0, -4.34765355285354, 
+-9.75360353139628, -16.5935673247779, -19.6317805370012, -16.4086037689043, 
+0, 9.00543772308487, 17.4182674984633, 24.7131215275189, 34.24041182839, 
+43.7067274538907), .Dim = c(6L, 2L), .Dimnames = list(NULL, c("coords.x1", 
+"coords.x2"))), bbox = structure(c(-19.6317805370012, 0, 0, 43.7067274538907
+), .Dim = c(2L, 2L), .Dimnames = list(c("coords.x1", "coords.x2"
+), c("min", "max"))), proj4string = new("CRS", projargs = "+proj=aea +lat_0=45.568977 +lon_0=-83.248627 +lat_1=42.122774 +lat_2=49.01518 +x_0=1000000 +y_0=1000000 +datum=NAD83 +units=m +no_defs"))
+
+path_dfin_dfout_shouldBe <- 
+structure(list(X = c(0, -4.34765355285354, -9.75360353139628, 
+-16.5935673247779, -19.6317805370012, -16.4086037689043), Y = c(0, 
+9.00543772308487, 17.4182674984633, 24.7131215275189, 34.24041182839, 
+43.7067274538907)), class = "data.frame", row.names = c("1", 
+"2", "3", "4", "5", "6"))
+
+
 # Testing output matches desired format for each input
 test_that("data.frame input, spatial output gives expected result", {
   # Check if expected and actual results are the same
-  expect_equal(sf::st_coordinates(path_dfin_sfout), path_dfin_sfout_coords_shouldBe)
+  expect_equal(sp::coordinates(path_dfin_spout), sp::coordinates(path_dfin_spout_coords_shouldBe))
 })
 test_that("data.frame input, data.frame output gives expected result", {
   # Check if expected and actual results are the same
@@ -67,5 +75,5 @@ test_that("spatial input, data.frame output gives expected result", {
 })
 test_that("spatial input, spatial output gives expected result", {
   # Check if expected and actual results are the same
-  expect_equal(sf::st_coordinates(path_spin_spout), path_spin_sfout_coords_shouldBe)
+  expect_equal(sp::coordinates(path_spin_spout), sp::coordinates(path_spin_spout_shouldBe))
 })
