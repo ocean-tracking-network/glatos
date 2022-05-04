858a430587265966302e6cf9027d8c6015f9af37
chrisholbrook <cholbrook@usgs.gov>
2022-05-04 17:00:48 -0400

    remove checks for GDAL installation

--- R/util-check_dependencies.r
+++ R/util-check_dependencies.r
@@ -3,13 +3,9 @@
 ##' Checks to make sure external dependencies required by functions in
 ##' 'glatos' are installed and available to R.
 ##'
-##' @details \code{check_dependencies} checks that the Geospatial Data
-##'   Abstraction Library (GDAL) and ffmpeg (cross platform software
+##' @details \code{check_dependencies} checks that ffmpeg (cross platform software
 ##'   for manipulating video content) software are installed on your
-##'   computer and accessible to R.  GDAL is used by the
-##'   \code{make_transition} function to create a transition layer
-##'   required by \code{interpolate_path} for non-linear
-##'   interpolation.  FFmpeg is required to create or modify video
+##'   computer and accessible to R. FFmpeg is required to create or modify video
 ##'   animations of fish movement using the \code{make_frames},
 ##'   \code{make_video}, and \code{adjust_video_playback} functions.
 ##'
@@ -18,19 +14,7 @@
 ##'   are installed and accessible, a message is returned to the
 ##'   terminal stating that the check was successful. Failed attempts
 ##'   to access the external libraries are printed to the terminal.
-##'
-##' @details Installation of the GDAL library and a number of other
-##'   open-source programs useful for working with spatial data on
-##'   windows is accomplished by downloading the network installer for
-##'   your appropriate windows computer (32 or 64 bit) at
-##'   \url{https://trac.osgeo.org/osgeo4w/}. Installation of the GDAL
-##'   library on Mac is possible using Homebrew or KyngChaos 3rd party
-##'   repositories.  Alternatively, the GDAL library is incorporated
-##'   in QGIS (open source desktop GIS) software and may be obtained
-##'   by installing QGIS.  Standalone installers for QGIS are
-##'   available (Windows, Mac, and Linux) at
-##'   \url{https://qgis.org/en/site/forusers/download.html}.
-##'
+##'   
 ##' @details The simplest way to install FFMPEG for use by the 'glatos' package
 ##'   functions is to use the \code{install_ffmpeg} function, which downloads
 ##'   the excecutable from one of the websites listed below (depending on
@@ -72,24 +56,6 @@
 
 check_dependencies <- function(){
   
-  # check for gdal
-  message("Checking for gdal...")
-  suppressWarnings(gdalUtils::gdal_setInstallation())
-  valid_install <- !is.null(getOption("gdalUtils_gdalPath"))
-  if(valid_install){
-    message(sprintf(" OK... gdal version %s is installed",
-                    getOption("gdalUtils_gdalPath")[[1]]$version), "\n")
-  } else {
-    message(" gdal not found.\n",
-              " To install gdal, see:\n",
-              "\t- http://www.gdal.org\n",
-              "\t- https://trac.osgeo.org/osgeo4w\n",
-              "\t- https://trac.osgeo.org/gdal/wiki/DownloadingGdalBinaries\n",
-              " or to install QGIS (gis software), see:\n",
-              "\t- https://qgis.org/en/site/forusers/download.html")
-  }
-  
-
   # check for ffmpeg
   message("Checking for ffmpeg...")
   ffmpeg <- tryCatch(list(found = TRUE, value = glatos:::get_ffmpeg_path(NA)), 
