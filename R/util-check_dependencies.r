0378f58b7d83051b39f95e7e4a5e3c9db9a74773
chrisholbrook <cholbrook@usgs.gov>
2024-01-04 16:11:43 -0500

    defunct check_dependencies

--- R/util-check_dependencies.r
+++ R/util-check_dependencies.r
@@ -1,17 +1,8 @@
 #' @export
-check_dependencies <- function(){
+check_dependencies <- function(...){
   
-  # check for ffmpeg
-  message("Checking for ffmpeg...")
-  ffmpeg <- tryCatch(list(found = TRUE, value = glatos:::get_ffmpeg_path(NA)), 
-                     error = function(e) list(found = FALSE, value = e$message))
-                         
-  
-  # print message with result
-  if(ffmpeg$found) { 
-    message(" OK... FFmpeg is installed at \n  ", ffmpeg$value, ".", "\n") 
-  } else {
-    message(ffmpeg$value, "\n")
-  }
+  # FFmpeg is no longer needed.  Function is now defunct
+  .Defunct(new = NA, 
+           msg = "ffmpeg dependency is no longer needed by glatos package")
   
 }
