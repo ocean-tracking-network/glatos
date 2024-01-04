5f3af210816fbf665638ed2bad997de27f2fa476
chrisholbrook <cholbrook@usgs.gov>
2024-01-04 16:03:15 -0500

    remove links from defunct doc

--- R/package-glatos-defunct.r
+++ R/package-glatos-defunct.r
@@ -3,12 +3,12 @@
 #' These functions are gone, no longer available.
 #'
 #' \itemize{
-#' \item \code{\link{check_dependencies}}: Removed in glatos 0.7.0.
+#' \item \code{check_dependencies}: Removed in glatos 0.7.0.
 #'
-#' \item \code{\link{make_video_ffmpeg}}: Removed in glatos 0.7.0. 
+#' \item \code{make_video_ffmpeg}: Removed in glatos 0.7.0. 
 #' Use \code{\link{make_video}} instead.
 #'
-#' \item \code{\link{install_ffmpeg}}: Removed in glatos 0.7.0.
+#' \item \code{install_ffmpeg}: Removed in glatos 0.7.0.
 #'
 #' }
 #'
