f9cded3d41de9f30a088a95aabc7bcc9912df941
Mike O'Brien <mikeob9@gmail.com>
2024-01-12 13:25:43 -0500

    testthat3e convert_glatos_to_att

--- tests/testthat/test-convert_glatos_to_att.r
+++ tests/testthat/test-convert_glatos_to_att.r
@@ -1,28 +1,33 @@
-context("Check convert_glatos_to_att")
-
 # check against internal data object 'walleye_att' in R/sysdata.r
 
 # Actual result
 # get path to example detection file
 wd_file <- system.file("extdata",
-  "walleye_detections.csv",
-  package = "glatos"
+                       "walleye_detections.csv",
+                       package = "glatos"
 )
 
 wald <- read_glatos_detections(wd_file)
 
 # get path to example receiver file
 rec_file <- system.file("extdata", "sample_receivers.csv",
-  package = "glatos"
+                        package = "glatos"
 )
 recd <- read_glatos_receivers(rec_file) # load receiver data
 
-watt <- convert_glatos_to_att(wald, recd)
 
+test_that("matches internal data: walleye_att", {
+  expect_no_error(
+    watt <- convert_glatos_to_att(wald, recd)
+  )
 
-
-# Test using testthat library
-test_that("walleye_att gives expected result", {
   # Check if expected and actual results are the same
-  expect_equal(watt, walleye_att)
+  expect_identical(watt, walleye_att)
+})
+
+test_that('matches type/class of internal data: walleye_att', {
+  watt <- convert_glatos_to_att(wald, recd)
+
+  expect_s3_class(watt, 'ATT')
+  expect_type(watt, 'list')
 })
