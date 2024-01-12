00f6f3f9e2be44c8eb4bc30a86c445adbb695820
Mike O'Brien <mikeob9@gmail.com>
2024-01-12 12:15:39 -0500

    adjust_playback_time testthat3e

--- tests/testthat/test-adjust_playback_time.r
+++ tests/testthat/test-adjust_playback_time.r
@@ -3,72 +3,94 @@ frames <- system.file("extdata", "frames", package = "glatos")
 
 # make video animation
 temp_dir <- tempdir()
-temp_file <- tempfile(fileext = ".mp4")
-make_video(
+
+temp_file <- make_video(
   input_dir = frames,
   intput_ext = ".png",
-  output = temp_file
+  output = tempfile(fileext = ".mp4")
 )
-
-# slow video down by a factor of 10
-temp_file_1 <- tempfile(fileext = ".mp4")
-adjust_playback_time(
-  scale_factor = 10,
-  input = temp_file,
-  output_dir = temp_dir,
-  output = basename(temp_file_1)
-)
-
-# speed up by a factor of 10
-temp_file_2 <- tempfile(fileext = ".mp4")
-adjust_playback_time(
-  scale_factor = 0.1,
-  input = temp_file,
-  output_dir = temp_dir,
-  output = basename(temp_file_2)
-)
-
-# call from input path and name containing spaces and parentheses
-temp_dir_3 <- file.path(temp_dir, "path with ( spaces)")
-dir.create(temp_dir_3)
-temp_file_3 <- tempfile(
-  pattern = "fname with ( special)",
-  tmpdir = temp_dir_3,
-  fileext = ".mp4"
-)
-adjust_playback_time(
-  scale_factor = 0.1,
-  input = temp_file,
-  output_dir = temp_dir_3,
-  output = basename(temp_file_3)
-)
-
-# Actual file sizes
-vid_size <- file.info(c(temp_file_1, temp_file_2, temp_file_3))$size
-# round to MB
-vid_size <- round(vid_size * 0.001)
-
-# Expected file sizes
-size_should_be <- round(c(93158, 56817, 56817) * 0.001)
-
-# Clean up
-unlink(list.files(temp_dir,
-  full.names = TRUE, recursive = TRUE,
-  include.dirs = TRUE
-), recursive = TRUE)
 
 # Testing file size results
+# slow video down by a factor of 10
 test_that("slow down gives expected result", {
+  temp_file_1 <- tempfile(fileext = ".mp4")
+
+  # check no error
+  expect_no_error(
+    slow_down <- adjust_playback_time(
+      scale_factor = 10,
+      input = temp_file,
+      output_dir = temp_dir,
+      output = basename(temp_file_1)
+    )
+  )
+
   # Check if expected and actual file sizes
-  expect_equal(vid_size[1], size_should_be[1])
+  expect_equal(
+    file.size(slow_down),
+    93158,
+    tolerance = 0.005 # 0.5% difference
+  )
 })
 
+
+# speed up by a factor of 10
 test_that("speed up gives expected result", {
+  temp_file_2 <- tempfile(fileext = ".mp4")
+
+  expect_no_error(
+    speed_up <- adjust_playback_time(
+      scale_factor = 0.1,
+      input = temp_file,
+      output_dir = temp_dir,
+      output = basename(temp_file_2)
+    )
+  )
+
   # Check if expected and actual file sizes
-  expect_equal(vid_size[2], size_should_be[2])
+  expect_equal(
+    file.size(speed_up),
+    56817,
+    tolerance = 0.005 # 0.5% difference
+  )
 })
 
+# call from input path and name containing spaces and parentheses
 test_that("input/output with space/parenth gives expected result", {
+  temp_dir_3 <- file.path(temp_dir, "path with ( spaces)")
+  dir.create(temp_dir_3)
+
+  temp_file_3 <- tempfile(
+    pattern = "fname with ( special)",
+    tmpdir = temp_dir_3,
+    fileext = ".mp4"
+  )
+
+  expect_no_error(
+    path_spaces <- adjust_playback_time(
+      scale_factor = 0.1,
+      input = temp_file,
+      output_dir = temp_dir_3,
+      output = basename(temp_file_3)
+    )
+  )
+
   # Check if expected and actual file sizes
-  expect_equal(vid_size[3], size_should_be[3])
+  expect_equal(
+    file.size(path_spaces),
+    56817,
+    tolerance = 0.005
+  )
 })
+
+
+# Clean up
+unlink(
+  list.files(temp_dir,
+             full.names = TRUE,
+             recursive = TRUE,
+             include.dirs = TRUE
+  ),
+  recursive = TRUE
+)
+
