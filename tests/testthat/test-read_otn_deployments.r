2b1d07dd4e2aed5075cff71976711d245defa5ee
Mike O'Brien <mikeob9@gmail.com>
2024-01-13 19:11:49 -0500

    testthat3e read_otn_deployments

--- tests/testthat/test-read_otn_deployments.r
+++ tests/testthat/test-read_otn_deployments.r
@@ -1,16 +1,12 @@
-context("Check read_otn_deployments")
-
-# Actual result
-# get path to example deployment file
-dep_file <- system.file("extdata",
-  "hfx_deployments.csv",
-  package = "glatos"
-)
-
-deps <- read_otn_deployments(dep_file)
-
-# Test using testthat library
 test_that("hfx_deployments gives expected result", {
+  dep_file <- system.file("extdata",
+                          "hfx_deployments.csv",
+                          package = "glatos"
+  )
+
+  deps <- read_otn_deployments(dep_file)
+
+
   # Check if expected and actual results are the same
   expect_equal(deps, hfx_deployments)
 })
