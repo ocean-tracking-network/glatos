214273181f5e999a4830700f9df2edf0fce16322
mhpob <mhpob@users.noreply.github.com>
2024-01-22 17:19:01 +0000

    Style code (GHA)

--- tests/testthat/test-make_frames.r
+++ tests/testthat/test-make_frames.r
@@ -3,19 +3,19 @@ pos <- interpolate_path(
   walleye_detections[walleye_detections$animal_id == 153, ][
     1:125,
   ],
-  start_time = as.POSIXct('2012-04-29 13:05:27')
+  start_time = as.POSIXct("2012-04-29 13:05:27")
 )[1:4, ]
 
 
 test_that("Expected result when background lims and map not supplied", {
   # Check output with default background_ylim, background_xlim, and bg_map
   temp_dir <- tempdir()
-  
+
   expect_message(
     make_frames(pos, out_dir = temp_dir, preview = TRUE),
-    'Preview frames written to'
+    "Preview frames written to"
   )
-  
+
   # Check if expected and actual file sizes
   expect_equal(
     file.size(
@@ -24,7 +24,7 @@ test_that("Expected result when background lims and map not supplied", {
     30919,
     tolerance = 0.01
   )
-  
+
   # Clean up
   unlink(
     list.files(
@@ -35,22 +35,21 @@ test_that("Expected result when background lims and map not supplied", {
     ),
     recursive = TRUE
   )
-  
 })
 
 test_that("Expected result when map but not background lims supplied", {
   temp_dir <- tempdir()
-  
+
   expect_message(
     make_frames(
       pos,
-      out_dir = temp_dir, 
+      out_dir = temp_dir,
       preview = TRUE,
       bg_map = great_lakes_polygon
     ),
-    'Preview frames written to'
+    "Preview frames written to"
   )
-  
+
   # Check if expected and actual file sizes
   expect_equal(
     file.size(
@@ -59,7 +58,7 @@ test_that("Expected result when map but not background lims supplied", {
     24300,
     tolerance = 0.01
   )
-  
+
   # Clean up
   unlink(
     list.files(
@@ -70,24 +69,22 @@ test_that("Expected result when map but not background lims supplied", {
     ),
     recursive = TRUE
   )
-  
 })
 
 test_that("Expected result when map and background lims supplied", {
-  
   temp_dir <- tempdir()
-  
+
   expect_message(
     make_frames(pos,
-                out_dir = temp_dir,
-                background_ylim = c(42, 47),
-                background_xlim = c(-90, -78),
-                preview = TRUE,
-                bg_map = great_lakes_polygon
+      out_dir = temp_dir,
+      background_ylim = c(42, 47),
+      background_xlim = c(-90, -78),
+      preview = TRUE,
+      bg_map = great_lakes_polygon
     ),
-    'Preview frames written to'
+    "Preview frames written to"
   )
-  
+
   # Check if expected and actual file sizes
   expect_equal(
     file.size(
@@ -96,7 +93,7 @@ test_that("Expected result when map and background lims supplied", {
     32220,
     tolerance = 0.01
   )
-  
+
   # Clean up
   unlink(
     list.files(
@@ -107,26 +104,25 @@ test_that("Expected result when map and background lims supplied", {
     ),
     recursive = TRUE
   )
-  
 })
 
 test_that("Expected result when map is spatVector, bg lims not supplied", {
   sv_poly <- terra::vect(great_lakes_polygon)
-  
+
   temp_dir <- tempdir()
   expect_message(
     expect_message(
       make_frames(pos,
-                  out_dir = temp_dir,
-                  preview = TRUE,
-                  bg_map = sv_poly
+        out_dir = temp_dir,
+        preview = TRUE,
+        bg_map = sv_poly
       ),
-      'Converted terra object to sf'
+      "Converted terra object to sf"
     ),
-    'Preview frames written to'
+    "Preview frames written to"
   )
-  
-  
+
+
   expect_equal(
     file.size(
       file.path(temp_dir, "1.png")
@@ -134,8 +130,8 @@ test_that("Expected result when map is spatVector, bg lims not supplied", {
     24000,
     tolerance = 0.02
   )
-  
-  
+
+
   # Clean up
   unlink(
     list.files(
@@ -146,5 +142,4 @@ test_that("Expected result when map is spatVector, bg lims not supplied", {
     ),
     recursive = TRUE
   )
-  
 })
