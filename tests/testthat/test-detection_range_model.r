4ab6768b49c7c2da032bf3d6efe98e7d0be0c372
benjaminhlina <benjaminhlina@users.noreply.github.com>
2024-03-05 22:23:28 +0000

    Style code (GHA)

--- tests/testthat/test-detection_range_model.r
+++ tests/testthat/test-detection_range_model.r
@@ -2,16 +2,16 @@
 
 test_that("Test if polynomial model for model_frame = 'data_frame', is correct", {
   m <- detection_range_model(
-      avg_percent ~ -1 + distance_m + I(distance_m^2) +
+    avg_percent ~ -1 + distance_m + I(distance_m^2) +
 
-        I(distance_m^3) + offset(intercept),
-      data = sample_detection_efficiency,
-      p = c(50),
-      link = "polynomial",
-      model_frame = "data_frame",
-      summary_stats = TRUE
-    )
-  
+      I(distance_m^3) + offset(intercept),
+    data = sample_detection_efficiency,
+    p = c(50),
+    link = "polynomial",
+    model_frame = "data_frame",
+    summary_stats = TRUE
+  )
+
   expect_equal(m$distance, expected = 371)
   expect_equal(m$chi_square, expected = 1304, tolerance = 0.01)
   expect_equal(m$pgof, expected = 4.20e-281, tolerance = 0.01)
@@ -19,16 +19,16 @@ test_that("Test if polynomial model for model_frame = 'data_frame', is correct",
 })
 
 test_that("Test if polynomial model for model_frame = 'matrix', is correct", {
-  m1 <-  detection_range_model(
-      avg_percent ~ -1 + poly(distance_m, 3, raw = TRUE) +
+  m1 <- detection_range_model(
+    avg_percent ~ -1 + poly(distance_m, 3, raw = TRUE) +
 
-        offset(intercept),
-      data = sample_detection_efficiency,
-      p = c(50),
-      link = "polynomial",
-      model_frame = "matrix",
-      summary_stats = TRUE
-    )
+      offset(intercept),
+    data = sample_detection_efficiency,
+    p = c(50),
+    link = "polynomial",
+    model_frame = "matrix",
+    summary_stats = TRUE
+  )
   expect_equal(m1$distance, expected = 371)
   expect_equal(m1$chi_square, expected = 1304, tolerance = 0.01)
   expect_equal(m1$pgof, expected = 4.20e-281, tolerance = 0.01)
@@ -37,11 +37,11 @@ test_that("Test if polynomial model for model_frame = 'matrix', is correct", {
 
 test_that("Test if logit model is correct", {
   m2 <- detection_range_model(avg_percent_d ~ distance_m,
-      data = sample_detection_efficiency,
-      p = c(50),
-      link = "logit",
-      summary_stats = TRUE
-    )
+    data = sample_detection_efficiency,
+    p = c(50),
+    link = "logit",
+    summary_stats = TRUE
+  )
   expect_equal(m2$distance, expected = 369, tolerance = 0.01)
   expect_equal(m2$chi_square, expected = 0.751, tolerance = 0.01)
   expect_equal(m2$pgof, expected = 0.980, tolerance = 0.01)
@@ -51,12 +51,12 @@ test_that("Test if logit model is correct", {
 
 test_that("Test if probit model is correct", {
   m3 <- detection_range_model(avg_percent_d ~ distance_m,
-      data = sample_detection_efficiency,
-      p = c(50),
-      link = "probit",
-      summary_stats = TRUE
-    )
-  
+    data = sample_detection_efficiency,
+    p = c(50),
+    link = "probit",
+    summary_stats = TRUE
+  )
+
   expect_equal(m3$distance, expected = 364, tolerance = 0.01)
   expect_equal(m3$pgof, expected = 0.973, tolerance = 0.01)
   expect_equal(m3$chi_square, expected = 0.864, tolerance = 0.01)
