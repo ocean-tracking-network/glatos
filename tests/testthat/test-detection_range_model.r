43d8e74eaf507ff0da9debf4d4f5c0415af90b8d
benjaminhlina <benjaminhlina@users.noreply.github.com>
2024-03-07 22:53:41 +0000

    Style code (GHA)

--- tests/testthat/test-detection_range_model.r
+++ tests/testthat/test-detection_range_model.r
@@ -4,15 +4,16 @@ test_that("Test if polynomial model for model_frame = 'data_frame', is correct",
   expect_warning(
     m <- detection_range_model(
       avg_percent ~ -1 + distance_m + I(distance_m^2) +
-        
+
         I(distance_m^3) + offset(intercept),
       data = sample_detection_efficiency,
       p = c(50),
       link = "polynomial",
       model_frame = "data_frame",
       summary_stats = TRUE
-    ))
-  
+    )
+  )
+
   expect_equal(m$distance, expected = 371)
   expect_equal(m$chi_square, expected = 1304, tolerance = 0.01)
   expect_equal(m$pgof, expected = 4.20e-281, tolerance = 0.01)
@@ -23,7 +24,7 @@ test_that("Test if polynomial model for model_frame = 'matrix', is correct", {
   expect_warning(
     m1 <- detection_range_model(
       avg_percent ~ -1 + poly(distance_m, 3, raw = TRUE) +
-        
+
         offset(intercept),
       data = sample_detection_efficiency,
       p = c(50),
@@ -41,10 +42,10 @@ test_that("Test if polynomial model for model_frame = 'matrix', is correct", {
 test_that("Test if logit model is correct", {
   expect_warning(
     m2 <- detection_range_model(avg_percent_d ~ distance_m,
-                                data = sample_detection_efficiency,
-                                p = c(50),
-                                link = "logit",
-                                summary_stats = TRUE
+      data = sample_detection_efficiency,
+      p = c(50),
+      link = "logit",
+      summary_stats = TRUE
     )
   )
   expect_equal(m2$distance, expected = 369, tolerance = 0.01)
@@ -57,12 +58,11 @@ test_that("Test if logit model is correct", {
 test_that("Test if probit model is correct", {
   expect_warning(
     m3 <- detection_range_model(avg_percent_d ~ distance_m,
-                                data = sample_detection_efficiency,
-                                p = c(50),
-                                link = "probit",
-                                summary_stats = TRUE
+      data = sample_detection_efficiency,
+      p = c(50),
+      link = "probit",
+      summary_stats = TRUE
     )
-    
   )
   expect_equal(m3$distance, expected = 364, tolerance = 0.01)
   expect_equal(m3$pgof, expected = 0.973, tolerance = 0.01)
