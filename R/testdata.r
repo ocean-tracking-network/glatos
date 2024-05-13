063848315655c39d80ba08018b6a808729cb7d11
Mike O'Brien <mikeob9@gmail.com>
2024-05-13 12:58:44 -0400

    example -> exampleS

--- R/testdata.r
+++ R/testdata.r
@@ -10,7 +10,7 @@
 #'
 #' @section Filename: higgins_lake_transition.rds
 #'
-#' @example
+#' @examples
 #' system.file("testdata", "higgins_lake_transition.rds", package = "glatos")
 #'
 #' @author Chris Holbrook
@@ -29,7 +29,7 @@ NULL
 #'
 #' @section Filename: flynn_island_transition.rds
 #'
-#' @example
+#' @examples
 #' system.file("testdata", "flynn_island_transition.rds", package = "glatos")
 #'
 #' @author Chris Holbrook
