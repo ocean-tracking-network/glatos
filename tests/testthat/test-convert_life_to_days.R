84b8664414d29d34d718bac1b424fd0490f4fb50
mhpob <mhpob@users.noreply.github.com>
2024-08-27 14:33:38 +0000

    Style code (GHA)

--- tests/testthat/test-convert_life_to_days.R
+++ tests/testthat/test-convert_life_to_days.R
@@ -1,20 +1,20 @@
 test_that("converts number stored as text", {
   life_text <- convert_life_to_days("5")
-  
+
   expect_equal(life_text, 5)
   expect_type(life_text, "integer")
 })
 
 test_that("converts number stored as numeric", {
   life_numeric <- convert_life_to_days(5)
-  
+
   expect_equal(life_numeric, 5)
   expect_type(life_numeric, "integer")
 })
 
 test_that("strips 'days'", {
   life_days <- convert_life_to_days("10 days")
-  
+
   expect_equal(life_days, 10)
   expect_type(life_days, "integer")
 })
@@ -24,7 +24,7 @@ test_that("errors if different unit", {
     convert_life_to_days("10 weeks"),
     "Cannot convert 10 weeks to time days."
   )
-  
+
   expect_error(
     convert_life_to_days("parsecs"),
     "Cannot convert parsecs to time days."
