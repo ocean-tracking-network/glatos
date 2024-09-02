9da125ce17c45c15e91e7e0b6a8512fc87628452
chrisholbrook <cholbrook@usgs.gov>
2024-09-02 00:35:56 -0400

    add tests for vue and vdat load functions

---
+++ tests/testthat/test-read_vdat_csv.R
@@ -0,0 +1,59 @@
+# R/load-read_vdat_csv.r
+
+test_that("read_vdat_csv works", {
+  
+  # VR2W file
+  vrl_file <- system.file("extdata", "detection_files_raw",
+    "VR2W_109924_20110718_1.vrl",
+    package = "glatos"
+  )
+
+  temp_dir <- tempdir()
+
+  csv_file <- suppressMessages(vdat_convert(vrl_file, 
+                                            out_dir = temp_dir, 
+                                            show_progress = FALSE))
+
+  # read all record types
+  expect_snapshot(vdat <- read_vdat_csv(csv_file))
+    
+
+  # read only DET record type
+  expect_equal(read_vdat_csv(csv_file, record_types = c("DET"))$DET,
+               vdat$DET)
+  
+})
+
+
+test_that("bad inputs are caught", {
+  
+  # Input file not found
+  not_a_file <- tempfile()
+  expect_warning(read_vdat_csv(not_a_file),
+                "File not found:")
+  
+  
+  # VUE export format
+  vue_csv_file <- system.file("extdata", "VR2W_109924_20110718_1.csv",
+                              package = "glatos")
+  
+  expect_error(read_vdat_csv(vue_csv_file),
+               "Input file appears to be in VUE Export format")
+  
+  
+  # Specified record_type not found in VDAT CSV
+  temp_file <- tempfile()
+  
+  vdat2 <- c(
+    "VEMCO DATA LOG,2.0.0,vdat-9.3.0-20240207-74ad8e-release",
+    "RECORD TYPE,FIELD,FIELD,FIELD,FIELD,FIELD,FIELD,FIELD",
+    "UNRECOGNIZED_DESC,Device Time (UTC),Time",
+    "UNRECOGNIZED,2020-01-01 01:01:01,2020-01-01 01:01:01") 
+
+  writeLines(vdat2, temp_file)
+  
+  expect_error(read_vdat_csv(temp_file, record_types = "DET"),
+               "The following input 'record_types' were not found in CSV file")
+  
+})
+
---
+++ tests/testthat/test-read_vue_event_csv.R
@@ -0,0 +1,39 @@
+# R/load-read_vue_event_csv.r
+
+test_that("read_vue_event_csv works", {
+  
+  # VR2W file
+  vue_event_csv_file <- system.file("extdata",
+                                    "VR2W_receiverEvents_109924_20110718_1.csv",
+                                    package = "glatos")
+
+  # read all record types
+  expect_snapshot(read_vue_event_csv(vue_event_csv_file))
+  
+})
+
+
+test_that("bad inputs are caught", {
+  
+  # Input file not found
+  not_a_file <- tempfile()
+  expect_warning(read_vue_event_csv(not_a_file),
+                "File not found:")
+
+
+  # VUE CSV is missing columns
+  vue_event_csv_file <- system.file("extdata",
+                                  "VR2W_receiverEvents_109924_20110718_1.csv",
+                                  package = "glatos")
+
+  vue_events <- read_vue_event_csv(vue_event_csv_file)
+
+  temp_file <- tempfile()
+
+  write.csv(vue_events[1:5,-2], temp_file, row.names = FALSE)
+
+  expect_error(read_vue_event_csv(temp_file),
+               "Input file does not appear to be in VUE Export format")
+  
+})
+
