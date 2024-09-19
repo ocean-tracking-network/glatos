95dde14fbc98e249e5daf064f2a69008a95da9d5
chrisholbrook <cholbrook@usgs.gov>
2024-09-18 22:50:17 -0400

    update readme and use as package help (as child in md)

--- R/package-glatos.r
+++ R/package-glatos.r
@@ -1,119 +1,8 @@
-#' An R package for the Great Lakes Acoustic Telemetry Observation System
-#'
-#' `glatos` is an R package with functions useful to members of the Great
-#' Lakes Acoustic Telemetry Observation System (<https://glatos.glos.us>).
-#' Functions may be generally useful for processing, analyzing, simulating, and
-#' visualizing acoustic telemetry data, but are not strictly limited to acoustic
-#' telemetry applications.
-#'
-#' @section Package status: *This package is in early development and its
-#'   content is evolving.* To access the package or contribute code, join the
-#'   project at (<https://github.com/ocean-tracking-network/glatos>). If you
-#'   encounter problems or have questions or suggestions, please post a new
-#'   issue or email <cholbrook@usgs.gov> (maintainer: Chris Holbrook).
-#'
-#' @section Installation: Installation instructions can be found at
-#'   <https://github.com/ocean-tracking-network/glatos/wiki/installation-instructions>.
-#'
-#'
-#'
-#' @section Data loading and processing: \describe{
-#'
-#'   \item{[read_glatos_detections] and [read_otn_detections]}{ Fast
-#'   data loading from standard GLATOS and OTN data files to a single structure
-#'   that is compatible with other glatos functions.}
-#'
-#'   \item{[read_glatos_receivers] and [read_otn_deployments]}{
-#'   Reads receiver location histories from standard GLATOS and OTN data files
-#'   to a single structure that is compatible with other glatos functions.}
-#'
-#'   \item{[read_glatos_workbook]}{ Reads project-specific receiver history
-#'   and fish taggging and release data from a standard glatos workbook file.}
-#'
-#'   \item{[read_vemco_tag_specs]}{ Reads transmitter (tag) specifications
-#'   and operating schedule.}
-#'
-#'   \item{[real_sensor_values]}{ Converts 'raw' transmitter sensor (e.g.,
-#'   depth, temperature) to 'real'-scale values (e.g., depth in meters) using
-#'   transmitter specification data (e.g., from [read_vemco_tag_specs]).} }
-#'
-#' @section Filtering and summarizing: \describe{
-#'   \item{[min_lag]}{
-#'   Facilitates identification and removal of false positive detections by
-#'   calculating the minimum time interval (min_lag) between successive
-#'   detections.}
-#'
-#'  \item{[false_detections]}{ Removes potential false positive detections
-#'  using "short interval" criteria (see [min_lag]).}
-#'
-#'  \item{[detection_events]}{ Distills detection data down to a much
-#'  smaller number of discrete detection events, defined as a change in location
-#'  or time gap that exceeds a threshold.}
-#'
-#'  \item{[summarize_detections]}{ Calculates number of fish detected,
-#'  number of detections, first and last detection timestamps, and/or mean
-#'  location of receivers or groups, depending on specific type of summary
-#'  requested.}
-#'
-#'  \item{[residence_index]}{ calculates the relative proportion of time
-#'  spent at each location.}
-#'
-#'  \item{[REI]}{ calculates the relative activity at each receiver based
-#'  on number of unique species and individual animals.}
-#' }
-#'
-#' @section Visualization and data exploration: \describe{
-#' \item{[abacus_plot]}{ Useful for exploring movement patterns of
-#' individual tagged animals through time.}
-#'
-#' \item{[detection_bubble_plot]}{ Useful for exploring distribution of
-#' tagged individuals among receivers.}
-#'
-#' \item{[interpolate_path], [make_frames], and [make_video]}{
-#' Interpolate spatio-temporal movements, between detections, create video
-#' frames, and stitch frames together to create animated video file using FFmpeg
-#' software.}
-#'
-#' \item{[adjust_playback_time]}{ Modify playback speed of videos and
-#' optionally convert between video file formats. Requires FFmpeg.}
-#' }
-#'
-#' @section Simulation functions for system design and evaluation: \describe{
-#' \item{[calc_collision_prob]}{ Estimates the probability of collisions
-#' for pulse-position-modulation type co-located telemetry transmitters. This is
-#' useful for determining the number of fish to release or tag specifications
-#' (e.g., delay).}
-#'
-#' \item{[receiver_line_det_sim]}{ Simulates detection of acoustic-tagged
-#' fish crossing a receiver line (or single receiver). This is useful for
-#' determining optimal spacing of receviers in a line and tag specifications
-#' (e.g., delay).}
-#'
-#' \item{[crw_in_polygon], [transmit_along_path], and
-#' [detect_transmissions]}{ Individually simulate random fish movement paths
-#' within a water body ([crw_in_polygon]: a random walk in a polygon), tag
-#' signal transmissions along those paths ([transmit_along_path]: time series
-#' and locations of transmissions based on tag specs), and detection of those
-#' transmittions by receivers in a user-defined receiver network
-#' ([detect_transmissions]: time series and locations of detections based on
-#' detection range curve). Collectively, these functions can be used to explore,
-#' compare, and contrast theoretical performance of a wide range of transmitter
-#' and receiver network designs.}
-#' }
-#'
-#' @section Convert glatos data objects to other package classes: \describe{
-#' \item{[convert_glatos_to_att]}{ Converts glatos_detections and
-#' glatos_receiver objects to ATT for compatibility with the Animal Tracking
-#' Toolbox(https://github.com/vinayudyawer/ATT) and the VTrack package.}
-#'
-#' \item{[convert_otn_erddap_to_att]}{ Converts glatos_detections and
-#' transmitter, receiver, and animal metadata from the OTN ERDDAP to ATT format
-#' for compatibility with the Animal Tracking
-#' Toolbox(https://github.com/vinayudyawer/ATT) and the VTrack package.}
-#' }
-
+#' ```{r child = "ReadMe.md"}
+#' ```
 #' @docType package
 #' @name glatos
+#' 
 #' @import data.table sp
 #' @importFrom graphics abline axis box legend lines mtext par points symbols
 #'   text
@@ -129,10 +18,13 @@
 # avoid R CMD check note
 globalVariables(".")
 
+# current nickname
+nickname <- function() "very-refreshing-lemonade"
+
 # package startup message
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(paste0(
     "version ", utils::packageVersion("glatos"),
-    " ('very-refreshing-lemonade')"
+    " ('", nickname(), "')"
   ))
 }
--- ReadMe.md
+++ ReadMe.md
@@ -1,15 +1,37 @@
 glatos: An R package for the Great Lakes Acoustic Telemetry Observation System
 ------------------------------------------------------------------------------
 
-glatos is an R package with functions useful to members of the Great Lakes Acoustic Telemetry Observation System (<http://glatos.glos.us>). Functions may be generally useful for processing, analyzing, simulating, and visualizing acoustic telemetry data, but are not strictly limited to acoustic telemetry applications.
+glatos is an R package with functions useful to members of the Great Lakes
+Acoustic Telemetry Observation System [https://glatos.glos.us]. Functions may
+be generally useful for processing, analyzing, simulating, and visualizing
+acoustic telemetry data, but are not strictly limited to acoustic telemetry
+applications.
 
 ### Package status
 
-*This package is in early development and its contents are evolving.* For recent changes, see [NEWS](https://github.com/ocean-tracking-network/glatos/blob/main/NEWS.md) for recent changes. To access the package or contribute code, join the project at https://github.com/ocean-tracking-network/glatos. If you encounter problems or have questions or suggestions, please post a new issue or email <cholbrook@usgs.gov> (maintainer: Chris Holbrook).
+glatos is hosted by the Ocean Tracking Network on [github](https://github.com/ocean-tracking-network/glatos]).
+
+* For recent changes, see
+[NEWS](https://github.com/ocean-tracking-network/glatos/blob/main/NEWS.md). 
+
+* To report a bug, ask a question, or propose something new, submit an [Issue](https://github.com/ocean-tracking-network/glatos/issues) or email the 
+maintainer (Chris Holbrook): <cholbrook@usgs.gov>.
+
 
 ### Installation
 
-Installation instructions can be found at https://github.com/ocean-tracking-network/glatos/wiki/installation-instructions
+To install the latest release (
+`r paste0("version ",utils::packageVersion("glatos")," '", nickname(), "'")`): 
+  
+```{r eval = FALSE}
+library(remotes) # for install_github
+install_github('ocean-tracking-network/glatos', build_vignettes = TRUE)
+```
+
+To install the development version, an earlier version, or to see frequently
+asked questions about installation, see
+[https://github.com/ocean-tracking-network/glatos/wiki/installation-instructions]
+
 
 ### Contents
 
@@ -27,12 +49,23 @@ Installation instructions can be found at https://github.com/ocean-tracking-netw
 
 6. [`prepare_tag_sheet`](https://github.com/ocean-tracking-network/glatos/blob/main/R/load-prepare_tag_sheet.r) and [`prepare_deploy_sheet`](https://github.com/ocean-tracking-network/glatos/blob/main/R/load-prepare_deploy_sheet.r) load OTN metadata sheets for Tagging and Deployment of Receivers and formats them for converting to ATT Data.
 
+7. `vue_convert` and `vdat_convert` extracts data from proprietary receiver
+files (.vrl, .vdat) using Innovasea's VUE and VDAT software (packaged with
+Fathom Connect software).
+
+8. `read_vdat_csv` reads data from a CSV file produced by Innovasea's Fathom
+Connect or VDAT software (or with `vdat_convert()`). Data from an "interleaved"
+(not "split") Fathom CSV format are read into R as a `vdat_list` object.
+
+9. `read_vue_detection_csv`, and `read_vue_event_csv` read data from a CSV file 
+exported from Innovasea's VUE software.
+
 
 #### Filtering and summarizing
 
 1.  [`min_lag`](https://github.com/ocean-tracking-network/glatos/blob/main/R/proc-min_lag.r) facilitates identification and removal of false positive detections by calculating the minimum time interval (min\_lag) between successive detections.
 
-2.  [`detection_filter`](https://github.com/ocean-tracking-network/glatos/blob/main/R/filt-false_detections.r) removes potential false positive detections using "short interval" criteria (see *min\_lag*).
+2.  [`false_detections`](https://github.com/ocean-tracking-network/glatos/blob/main/R/filt-false_detections.r) removes potential false positive detections using "short interval" criteria (see *min\_lag*).
 
 3.  [`detection_events`](https://github.com/ocean-tracking-network/glatos/blob/main/R/summ-detection_events.r) distills detection data down to a much smaller number of discrete detection events, defined as a change in location or time gap that exceeds a threshold.
 
@@ -57,9 +90,9 @@ species and individual animals.
 
 2.  [`detection_bubble_plot`](https://github.com/ocean-tracking-network/glatos/blob/main/R/vis-detection_bubble_plot.r) is useful for exploring distribution of tagged individuals among receivers.
 
-3.  [`interpolate_path`](https://github.com/ocean-tracking-network/glatos/blob/main/R/vis-interpolate_path.r), [`make_frames`](https://github.com/ocean-tracking-network/glatos/blob/main/R/vis-make_frames.r), and [`make_video`](https://github.com/ocean-tracking-network/glatos/blob/main/R/vis-make_video.r) Interpolate spatio-temporal movements, between detections, create video frames, and stitch frames together to create animated video file using *FFmpeg* software.
+3.  [`interpolate_path`](https://github.com/ocean-tracking-network/glatos/blob/main/R/vis-interpolate_path.r), [`make_frames`](https://github.com/ocean-tracking-network/glatos/blob/main/R/vis-make_frames.r), and [`make_video`](https://github.com/ocean-tracking-network/glatos/blob/main/R/vis-make_video.r) Interpolate spatio-temporal movements, between detections, create video frames, and stitch frames together to create animated video file.
 
-4.  [`adjust_playback_time`](https://github.com/ocean-tracking-network/glatos/blob/main/R/vis-adjust_playback_time.r) modify playback speed of videos and optionally convert between video file formats. Requires *FFmpeg*
+4.  [`adjust_playback_time`](https://github.com/ocean-tracking-network/glatos/blob/main/R/vis-adjust_playback_time.r) modify playback speed of videos and optionally convert between video file formats.
 
 #### Data Exporting
 
