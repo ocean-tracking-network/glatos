a609e2f5a942f7b69d0057311cc2a6e346cb70e2
Mike O'Brien <mike@obrien.page>
2025-08-11 15:23:57 -0400

    read zipped OTN detection extracts

--- tests/testthat/test-read_otn_detections.r
+++ tests/testthat/test-read_otn_detections.r
@@ -14,3 +14,52 @@ test_that("blue_shark_detections gives expected result", {
   expect_s3_class(bsd, "glatos_detections")
   expect_s3_class(bsd, "data.frame")
 })
+
+test_that("handles zipped directory with one file", {
+  bsd_file <- system.file(
+    "extdata",
+    "blue_shark_detections.csv",
+    package = "glatos"
+  )
+
+  td <- file.path(tempdir(), "ziptest.zip")
+  zip::zip(td, bsd_file, mode = "cherry-pick")
+
+  bsd <- read_otn_detections(td)
+
+  # Check if expected and actual results are the same
+  expect_equal(bsd, blue_shark_detections)
+
+  expect_s3_class(bsd, "glatos_detections")
+  expect_s3_class(bsd, "data.frame")
+
+  unlink(td, recursive = TRUE)
+})
+
+test_that("handles zipped directory with multiple files", {
+  bsd_file <- system.file(
+    "extdata",
+    "blue_shark_detections.csv",
+    package = "glatos"
+  )
+
+  td <- file.path(tempdir(), "ziptest.zip")
+  temp_txt <- file.path(tempdir(), "data_description.txt")
+  file.create(temp_txt)
+  zip::zip(
+    td,
+    c(bsd_file, temp_txt),
+    mode = "cherry-pick"
+  )
+
+  bsd <- read_otn_detections(td)
+
+  # Check if expected and actual results are the same
+  expect_equal(bsd, blue_shark_detections)
+
+  expect_s3_class(bsd, "glatos_detections")
+  expect_s3_class(bsd, "data.frame")
+
+  unlink(td, recursive = TRUE)
+  unlink(temp_txt)
+})
