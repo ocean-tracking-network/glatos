11315daffdb144aeec93790796f086a920647e7d
haydento <haydento@users.noreply.github.com>
2026-08-27 20:53:17 +0000

    [GitHub Actions] Style code

--- R/summ-detection_range_model.r
+++ R/summ-detection_range_model.r
@@ -92,14 +92,14 @@
 #' @author Benjamin L. Hlina
 #' @examples
 #' sample_detection_efficiency <- tibble::tribble(
-#'  ~distance_m , ~avg_percent , ~std_dev , ~avg_percent_d , ~intercept ,
-#'   62.9       , 95.3         , 12.3     , 0.953          ,        100 ,
-#'  181         , 89.3         , 14.3     , 0.893          ,        100 ,
-#'  346         , 76.2         , 23.0     , 0.762          ,        100 ,
-#'  724         ,  0           ,  0       , 0              ,        100 ,
-#'  348         , 70.0         , 28.0     , 0.700          ,        100 ,
-#'  464         ,  0           ,  0       , 0              ,        100 ,
-#'  752         ,  0           ,  0       , 0              ,        100
+#'   ~distance_m, ~avg_percent, ~std_dev, ~avg_percent_d, ~intercept,
+#'   62.9, 95.3, 12.3, 0.953, 100,
+#'   181, 89.3, 14.3, 0.893, 100,
+#'   346, 76.2, 23.0, 0.762, 100,
+#'   724, 0, 0, 0, 100,
+#'   348, 70.0, 28.0, 0.700, 100,
+#'   464, 0, 0, 0, 100,
+#'   752, 0, 0, 0, 100
 #' )
 
 #' # third order polynomial: # ave_percent is a whole number
